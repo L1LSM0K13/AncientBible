7a3c31ab41bd1293ef64f33f8da0a92bdd890fa4
Omar <villalobosomar0@gmail.com>
2023-12-22 20:27:13 -0600

    Made parallel work more effeciently.

--- dist/index.js
+++ dist/index.js
@@ -2,14 +2,34 @@
 
 // Toggling Parallel Bible
 document.getElementById('parallelBtn').addEventListener('click', function() {
-  let parallelBlock = document.getElementById('parallelBlock');
-  let mainScripture = document.getElementById('mainScripture');
+  let scriptureBlock = document.getElementById('parallelToggle');
+  let parallelScripture = document.getElementById('parallelScripture')
 
-  if (parallelBlock.classList.contains('hidden')) {
-    parallelBlock.classList.remove('hidden');
-    parallelBlock.classList.add('grid', 'grid-cols-2');
+  if (scriptureBlock.classList.contains('grid-cols-2')) {
+    scriptureBlock.classList.remove('grid-cols-2');
   } else {
-    parallelBlock.classList.add('hidden');
-    parallelBlock.classList.remove('grid', 'grid-cols-2')
+    scriptureBlock.classList.add('grid-cols-2');
+  };
+
+  if(parallelScripture.classList.contains('hidden')) {
+    parallelScripture.classList.remove('hidden');
+  } else {
+    parallelScripture.classList.add('hidden');
   }
-});
+})
+
+
+// OLD
+
+// document.getElementById('parallelBtn').addEventListener('click', function() {
+//   let parallelBlock = document.getElementById('parallelBlock');
+//   let mainScripture = document.getElementById('mainScripture');
+
+//   if (parallelBlock.classList.contains('hidden')) {
+//     parallelBlock.classList.remove('hidden');
+//     parallelBlock.classList.add('grid', 'grid-cols-2');
+//   } else {
+//     parallelBlock.classList.add('hidden');
+//     parallelBlock.classList.remove('grid', 'grid-cols-2')
+//   }
+// });
