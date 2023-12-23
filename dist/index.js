f5c3de66d837847abacd327a0a181c41daa84050
Omar <villalobosomar0@gmail.com>
2023-12-22 20:01:14 -0600

    Added parallel function to bible page.

--- dist/index.js
+++ dist/index.js
@@ -1,54 +1,15 @@
 'use strict'
 
 // Toggling Parallel Bible
-function toggleParallel() {
-  let parallelBlock = document.getElementById("");
-  if (parallelBlock.style.display == hidden || parallelBlock.style.display == null) {
-    parallelBlock.style.display == "";
+document.getElementById('parallelBtn').addEventListener('click', function() {
+  let parallelBlock = document.getElementById('parallelBlock');
+  let mainScripture = document.getElementById('mainScripture');
+
+  if (parallelBlock.classList.contains('hidden')) {
+    parallelBlock.classList.remove('hidden');
+    parallelBlock.classList.add('grid', 'grid-cols-2');
   } else {
-    parallelBlock.style.display = "";
+    parallelBlock.classList.add('hidden');
+    parallelBlock.classList.remove('grid', 'grid-cols-2')
   }
-}
-
-
-
-// Book loading functions
-
-/**
- * @typedef {{
- * book: string,
- *  chapters: {
- *    chapter: string,
- *    verses: {
- *      verse: string,
- *      text: string,
- *    }[],
- *   }[],
- * }} Book
- */
-
-/**
- * Loads a book based on its filename (e.g. 'Luke.json)
- * @param {string} filename The book filename
- * @returns {Promise<Book>} The book that was read from JSON
-*/
-
-async function loadBook(filename) {
-  const resp = await fetch('dist/KJV Bible/' + filename)
-  return await resp.json()
-}
-
-// Example: Load the first book
-loadBook(books[i]).then(function (book) {
-  console.log(book)
-
-  const container = document.getElementById('verse-container');
-
-  const verses = book.chapters[0].verses;
-  for (const verse of verses) {
-    const elem = document.createElement('p');
-    elem.classList.add('verse');
-    elem.innerText = verse.verse + '. ' + verse.text;
-    container.appendChild(elem);
-  }
-})
+});
