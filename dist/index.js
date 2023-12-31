e93067cc85f04621223bbbaa725fd6164c2650e3
Omar <villalobosomar0@gmail.com>
2023-12-30 20:48:45 -0600

    Added the scripture.

--- dist/index.js
+++ dist/index.js
@@ -56,13 +56,144 @@ document.getElementById('formatBtn').addEventListener('click', function() {
   }
 })
 
-document.getElementById('redLetterBtn').addEventListener('click', function() {
-  alert('test');
+const books = [
+  "Genesis.json",
+  "Exodus.json",
+  "Leviticus.json",
+  "Numbers.json",
+  "Deuteronomy.json",
+  "Joshua.json",
+  "Ruth.json",
+  "Judges.json",
+  "1Samuel.json",
+  "2Samuel.json",
+  "1Kings.json",
+  "2Kings.json",
+  "1Chronicles.json",
+  "2Chronicles.json",
+  "Ezra.json",
+  "Nehemiah.json",
+  "Esther.json",
+  "Job.json",
+  "Psalms.json",
+  "Proverbs.json",
+  "Ecclesiastes.json",
+  "SongofSolomon.json",
+  "Isaiah.json",
+  "Jeremiah.json",
+  "Lamentations.json",
+  "Ezekiel.json",
+  "Daniel.json",
+  "Hosea.json",
+  "Joel.json",
+  "Amos.json",
+  "Obadiah.json",
+  "Jonah.json",
+  "Micah.json",
+  "Nahum.json",
+  "Habakkuk.json",
+  "Zephaniah.json",
+  "Haggai.json",
+  "Zechariah.json",
+  "Malachi.json",
+  "Matthew.json",
+  "Mark.json",
+  "Luke.json",
+  "John.json",
+  "Acts.json",
+  "Romans.json",
+  "1Corinthians.json",
+  "2Corinthians.json",
+  "Galatians.json",
+  "Ephesians.json",
+  "Philippians.json",
+  "Colossians.json",
+  "1Thessalonians.json",
+  "2Thessalonians.json",
+  "1Timothy.json",
+  "2Timothy.json",
+  "Titus.json",
+  "Philemon.json",
+  "Hebrews.json",
+  "James.json",
+  "1Peter.json",
+  "2Peter.json",
+  "1John.json",
+  "2John.json",
+  "3John.json",
+  "Jude.json",
+  "Revelation.json",
+  "Books.json",
+];
+
+/**
+ * @typedef {{
+*   book: string,
+*   chapters: {
+*     chapter: string,
+*     verses: {
+*       verse: string,
+*       text: string,
+*     }[],
+*   }[],
+* }} Book
+*/
+
+/**
+ * Loads a book based on its filename (e.g. "Luke.json")
+ * @param {string} filename The book filename
+ * @returns {Promise<Book>} The book that was read from JSON
+ */
+async function loadBook(filename) {
+  const resp = await fetch('/dist/books/' + filename)
+  return await resp.json()
+}
+
+async function loadParallel(filename) {
+  const resp = await fetch('/dist/books/' + filename)
+  return await resp.json()
+}
+
+// Loads main text
+loadBook(books[41]).then(function (book) {
+  console.log(book)
+
+  const container = document.getElementById('verseContainer0');
+
+  const verses = book.chapters[0].verses;
+  for (const verse of verses) {
+    const elem = document.createElement('p');
+    elem.classList.add('p-1');
+    elem.innerText = verse.verse + '. ' + verse.text;
+    container.appendChild(elem);
+  }
+
 })
 
-document.getElementById('verseBtn').addEventListener('click', function() {
-  alert('test');
+// Loads parallel text
+loadParallel(books[41]).then(function (book) {
+  console.log(book)
+
+  const container = document.getElementById('verseContainer1');
+
+  const verses = book.chapters[0].verses;
+  for (const verse of verses) {
+    const elem = document.createElement('p');
+    elem.classList.add('p-1');
+    elem.innerText = verse.verse + '. ' + verse.text;
+    container.appendChild(elem);
+  }
+
+  document.getElementById('BookTitleNumber').innerText = '';
 })
+
+
+
+
+
+
+
+
 // OLD, THIS IS FROM PARALLEL BRANCH
 
 // document.getElementById('parallelBtn').addEventListener('click', function() {
