99ea678dfbf2527edd328772e93a1c3e23aa6778
Omar <villalobosomar0@gmail.com>
2023-12-31 18:55:30 -0600

    Added bible selection
    Co-authored-by: Ian <termer@termer.net>

--- dist/index.js
+++ dist/index.js
@@ -1,143 +1,356 @@
-'use strict'
+"use strict";
 
 // Toggling Parallel Bible
-document.getElementById('parallelBtn').addEventListener('click', function() {
-  const mainText = document.getElementById('mainText');
-  const parallelText = document.getElementById('parallelText');
+document.getElementById("parallelBtn").addEventListener("click", function () {
+  const mainText = document.getElementById("mainText");
+  const parallelText = document.getElementById("parallelText");
 
-  if (!mainText.classList.contains('col-span-2') && !parallelText.classList.contains('hidden')) {
-    mainText.classList.add('col-span-2');
-    parallelText.classList.add('hidden');
+  if (
+    !mainText.classList.contains("col-span-2") &&
+    !parallelText.classList.contains("hidden")
+  ) {
+    mainText.classList.add("col-span-2");
+    parallelText.classList.add("hidden");
 
-    mainText.classList.add('xl:mx-52', 'lg:mx-40', 'md:mx-28', 'sm:mx-12');
-    mainText.classList.remove('xl:ml-52', 'lg:ml-40', 'md:ml-28', 'sm:ml-12');
+    mainText.classList.add("xl:mx-52", "lg:mx-40", "md:mx-28", "sm:mx-12");
+    mainText.classList.remove("xl:ml-52", "lg:ml-40", "md:ml-28", "sm:ml-12");
 
-    parallelText.classList.add('xl:mx-52', 'lg:mx-40', 'md:mx-28', 'sm:mx-12');
-    parallelText.classList.remove('xl:mr-52', 'lg:mr-40', 'md:mr-28', 'sm:mr-12');
+    parallelText.classList.add("xl:mx-52", "lg:mx-40", "md:mx-28", "sm:mx-12");
+    parallelText.classList.remove(
+      "xl:mr-52",
+      "lg:mr-40",
+      "md:mr-28",
+      "sm:mr-12"
+    );
   } else {
-    mainText.classList.remove('col-span-2');
-    parallelText.classList.remove('hidden');
+    mainText.classList.remove("col-span-2");
+    parallelText.classList.remove("hidden");
 
-    mainText.classList.remove('xl:mx-52', 'lg:mx-40', 'md:mx-28', 'sm:mx-12');
-    mainText.classList.add('xl:ml-52', 'lg:ml-40', 'md:ml-28', 'sm:ml-12');
+    mainText.classList.remove("xl:mx-52", "lg:mx-40", "md:mx-28", "sm:mx-12");
+    mainText.classList.add("xl:ml-52", "lg:ml-40", "md:ml-28", "sm:ml-12");
 
-    parallelText.classList.remove('xl:mx-52', 'lg:mx-40', 'md:mx-28', 'sm:mx-12');
-    parallelText.classList.add('xl:mr-52', 'lg:mr-40', 'md:mr-28', 'sm:mr-12');
+    parallelText.classList.remove(
+      "xl:mx-52",
+      "lg:mx-40",
+      "md:mx-28",
+      "sm:mx-12"
+    );
+    parallelText.classList.add("xl:mr-52", "lg:mr-40", "md:mr-28", "sm:mr-12");
   }
-})
+});
 
 // Toggle Format Menu
-document.getElementById('formatBtn').addEventListener('click', function() {
-  const selectMenu = document.getElementById('selectMenu');
-  const formatMenu = document.getElementById('formatMenu');
+document.getElementById("formatBtn").addEventListener("click", function () {
+  const selectMenu = document.getElementById("selectMenu");
+  const formatMenu = document.getElementById("formatMenu");
 
-  if(!selectMenu.classList.contains('col-span-2') && !formatMenu.classList.contains('hidden')) {
-    selectMenu.classList.add('col-span-2');
-    formatMenu.classList.add('hidden');
+  if (
+    !selectMenu.classList.contains("col-span-2") &&
+    !formatMenu.classList.contains("hidden")
+  ) {
+    selectMenu.classList.add("col-span-2");
+    formatMenu.classList.add("hidden");
 
-    selectMenu.classList.add('xl:mx-52', 'lg:mx-40', 'md:mx-28', 'sm:mx-12');
-    selectMenu.classList.remove('xl:ml-52', 'lg:ml-40', 'md:ml-28', 'sm:ml-12');
+    selectMenu.classList.add("xl:mx-52", "lg:mx-40", "md:mx-28", "sm:mx-12");
+    selectMenu.classList.remove("xl:ml-52", "lg:ml-40", "md:ml-28", "sm:ml-12");
 
-    formatMenu.classList.add('xl:mx-52', 'lg:mx-40', 'md:mx-28', 'sm:mx-12');
-    formatMenu.classList.remove('xl:mr-52', 'lg:mr-40', 'md:mr-28', 'sm:mr-12');
+    formatMenu.classList.add("xl:mx-52", "lg:mx-40", "md:mx-28", "sm:mx-12");
+    formatMenu.classList.remove("xl:mr-52", "lg:mr-40", "md:mr-28", "sm:mr-12");
 
-    document.getElementById('formatBtn').textContent = 'Show Formatting'
+    document.getElementById("formatBtn").innerText = "Show Formatting";
   } else {
-    selectMenu.classList.remove('col-span-2');
-    formatMenu.classList.remove('hidden');
+    selectMenu.classList.remove("col-span-2");
+    formatMenu.classList.remove("hidden");
 
-    selectMenu.classList.remove('xl:mx-52', 'lg:mx-40', 'md:mx-28', 'sm:mx-12');
-    selectMenu.classList.add('xl:ml-52', 'lg:ml-40', 'md:ml-28', 'sm:ml-12');
+    selectMenu.classList.remove("xl:mx-52", "lg:mx-40", "md:mx-28", "sm:mx-12");
+    selectMenu.classList.add("xl:ml-52", "lg:ml-40", "md:ml-28", "sm:ml-12");
 
-    formatMenu.classList.remove('xl:mx-52', 'lg:mx-40', 'md:mx-28', 'sm:mx-12');
-    formatMenu.classList.add('xl:mr-52', 'lg:mr-40', 'md:mr-28', 'sm:mr-12');
+    formatMenu.classList.remove("xl:mx-52", "lg:mx-40", "md:mx-28", "sm:mx-12");
+    formatMenu.classList.add("xl:mr-52", "lg:mr-40", "md:mr-28", "sm:mr-12");
 
-    document.getElementById('formatBtn').textContent = 'Hide Formatting'
+    document.getElementById("formatBtn").innerText = "Hide Formatting";
   }
-})
+});
 
 const books = [
-  "Genesis.json",
-  "Exodus.json",
-  "Leviticus.json",
-  "Numbers.json",
-  "Deuteronomy.json",
-  "Joshua.json",
-  "Ruth.json",
-  "Judges.json",
-  "1Samuel.json",
-  "2Samuel.json",
-  "1Kings.json",
-  "2Kings.json",
-  "1Chronicles.json",
-  "2Chronicles.json",
-  "Ezra.json",
-  "Nehemiah.json",
-  "Esther.json",
-  "Job.json",
-  "Psalms.json",
-  "Proverbs.json",
-  "Ecclesiastes.json",
-  "SongofSolomon.json",
-  "Isaiah.json",
-  "Jeremiah.json",
-  "Lamentations.json",
-  "Ezekiel.json",
-  "Daniel.json",
-  "Hosea.json",
-  "Joel.json",
-  "Amos.json",
-  "Obadiah.json",
-  "Jonah.json",
-  "Micah.json",
-  "Nahum.json",
-  "Habakkuk.json",
-  "Zephaniah.json",
-  "Haggai.json",
-  "Zechariah.json",
-  "Malachi.json",
-  "Matthew.json",
-  "Mark.json",
-  "Luke.json",
-  "John.json",
-  "Acts.json",
-  "Romans.json",
-  "1Corinthians.json",
-  "2Corinthians.json",
-  "Galatians.json",
-  "Ephesians.json",
-  "Philippians.json",
-  "Colossians.json",
-  "1Thessalonians.json",
-  "2Thessalonians.json",
-  "1Timothy.json",
-  "2Timothy.json",
-  "Titus.json",
-  "Philemon.json",
-  "Hebrews.json",
-  "James.json",
-  "1Peter.json",
-  "2Peter.json",
-  "1John.json",
-  "2John.json",
-  "3John.json",
-  "Jude.json",
-  "Revelation.json",
-  "Books.json",
+  {
+    filename: "Genesis.json",
+    title: "Genesis",
+  },
+  {
+    filename: "Exodus.json",
+    title: "Exodus",
+  },
+  {
+    filename: "Leviticus.json",
+    title: "Leviticus",
+  },
+  {
+    filename: "Numbers.json",
+    title: "Numbers",
+  },
+  {
+    filename: "Deuteronomy.json",
+    title: "Deuteronomy",
+  },
+  {
+    filename: "Joshua.json",
+    title: "Joshua",
+  },
+  {
+    filename: "Ruth.json",
+    title: "Ruth",
+  },
+  {
+    filename: "Judges.json",
+    title: "Judges",
+  },
+  {
+    filename: "1Samuel.json",
+    title: "1Samuel",
+  },
+  {
+    filename: "2Samuel.json",
+    title: "2Samuel",
+  },
+  {
+    filename: "1Kings.json",
+    title: "1 Kings",
+  },
+  {
+    filename: "2Kings.json",
+    title: "2 Kings",
+  },
+  {
+    filename: "1Chronicles.json",
+    title: "1 Chronicles",
+  },
+  {
+    filename: "2Chronicles.json",
+    title: "2 Chronicles",
+  },
+  {
+    filename: "Ezra.json",
+    title: "Ezra",
+  },
+  {
+    filename: "Nehemiah.json",
+    title: "Nehemiah",
+  },
+  {
+    filename: "Esther.json",
+    title: "Esther",
+  },
+  {
+    filename: "Job.json",
+    title: "Job",
+  },
+  {
+    filename: "Psalms.json",
+    title: "Psalms",
+  },
+  {
+    filename: "Proverbs.json",
+    title: "Proverbs",
+  },
+  {
+    filename: "Ecclesiastes.json",
+    title: "Ecclesiastes",
+  },
+  {
+    filename: "SongofSolomon.json",
+    title: "Song of Solomon",
+  },
+  {
+    filename: "Isaiah.json",
+    title: "Isaiah",
+  },
+  {
+    filename: "Jeremiah.json",
+    title: "Jeremiah",
+  },
+  {
+    filename: "Lamentations.json",
+    title: "Lamentations",
+  },
+  {
+    filename: "Ezekiel.json",
+    title: "Ezekiel",
+  },
+  {
+    filename: "Daniel.json",
+    title: "Daniel",
+  },
+  {
+    filename: "Hosea.json",
+    title: "Hosea",
+  },
+  {
+    filename: "Joel.json",
+    title: "Joel",
+  },
+  {
+    filename: "Amos.json",
+    title: "Amos",
+  },
+  {
+    filename: "Obadiah.json",
+    title: "Obadiah",
+  },
+  {
+    filename: "Jonah.json",
+    title: "Jonah",
+  },
+  {
+    filename: "Micah.json",
+    title: "Micah",
+  },
+  {
+    filename: "Nahum.json",
+    title: "Nahum",
+  },
+  {
+    filename: "Habakkuk.json",
+    title: "Habakkuk",
+  },
+  {
+    filename: "Zephaniah.json",
+    title: "Zephaniah",
+  },
+  {
+    filename: "Haggai.json",
+    title: "Haggai",
+  },
+  {
+    filename: "Zechariah.json",
+    title: "Zechariah",
+  },
+  {
+    filename: "Malachi.json",
+    title: "Malachi",
+  },
+  {
+    filename: "Matthew.json",
+    title: "Matthew",
+  },
+  {
+    filename: "Mark.json",
+    title: "Mark",
+  },
+  {
+    filename: "Luke.json",
+    title: "Luke",
+  },
+  {
+    filename: "John.json",
+    title: "John",
+  },
+  {
+    filename: "Acts.json",
+    title: "Acts",
+  },
+  {
+    filename: "Romans.json",
+    title: "Romans",
+  },
+  {
+    filename: "1Corinthians.json",
+    title: "1 Corinthians",
+  },
+  {
+    filename: "2Corinthians.json",
+    title: "2 Corinthians",
+  },
+  {
+    filename: "Galatians.json",
+    title: "Galatians",
+  },
+  {
+    filename: "Ephesians.json",
+    title: "Ephesians",
+  },
+  {
+    filename: "Philippians.json",
+    title: "Philippians",
+  },
+  {
+    filename: "Colossians.json",
+    title: "Colossians",
+  },
+  {
+    filename: "1Thessalonians.json",
+    title: "1 Thessalonians",
+  },
+  {
+    filename: "2Thessalonians.json",
+    title: "2 Thessalonians",
+  },
+  {
+    filename: "1Timothy.json",
+    title: "1 Timothy",
+  },
+  {
+    filename: "2Timothy.json",
+    title: "2 Timothy",
+  },
+  {
+    filename: "Titus.json",
+    title: "Titus",
+  },
+  {
+    filename: "Philemon.json",
+    title: "Philemon",
+  },
+  {
+    filename: "Hebrews.json",
+    title: "Hebrews",
+  },
+  {
+    filename: "James.json",
+    title: "James",
+  },
+  {
+    filename: "1Peter.json",
+    title: "1 Peter",
+  },
+  {
+    filename: "2Peter.json",
+    title: "2 Peter",
+  },
+  {
+    filename: "1John.json",
+    title: "1 John",
+  },
+  {
+    filename: "2John.json",
+    title: "2 John",
+  },
+  {
+    filename: "3John.json",
+    title: "3 John",
+  },
+  {
+    filename: "Jude.json",
+    title: "Jude",
+  },
+  {
+    filename: "Revelation.json",
+    title: "Revelation",
+  },
 ];
 
 /**
  * @typedef {{
-*   book: string,
-*   chapters: {
-*     chapter: string,
-*     verses: {
-*       verse: string,
-*       text: string,
-*     }[],
-*   }[],
-* }} Book
-*/
+ *   book: string,
+ *   chapters: {
+ *     chapter: string,
+ *     verses: {
+ *       verse: string,
+ *       text: string,
+ *     }[],
+ *   }[],
+ * }} Book
+ */
 
 /**
  * Loads a book based on its filename (e.g. "Luke.json")
@@ -145,92 +358,110 @@ const books = [
  * @returns {Promise<Book>} The book that was read from JSON
  */
 async function loadBook(filename) {
-  const resp = await fetch('/dist/books/' + filename)
-  return await resp.json()
-}
+  const resp = await fetch("/dist/books/" + filename);
 
-async function loadParallel(filename) {
-  const resp = await fetch('/dist/books/' + filename)
-  return await resp.json()
-}
-
-// Loads main text
-loadBook(books[41]).then(function (book) {
-  console.log(book)
-
-  const container = document.getElementById('verseContainer0');
-
-  const verses = book.chapters[0].verses;
-  for (const verse of verses) {
-    const elem = document.createElement('p');
-    elem.classList.add('p-1');
-    elem.innerText = verse.verse + '. ' + verse.text;
-    container.appendChild(elem);
+  if (resp.status !== 200) {
+    throw new Error("Failed to fetch book, got HTTP status " + resp.status);
   }
 
-})
+  return await resp.json();
+}
+
+/**
+ * The currently selected book, or null if none
+ * @type {Book | null}
+ */
+let currentBook = null;
+
+const bookSelector = document.getElementById("selectBook");
+const chapterSelector = document.getElementById("selectChapter");
+
+/**
+ * Loads and displays a book by its filename
+ * @param {string} fileName The book's filename (e.g. "James.json")
+ */
+async function selectBookByFileName(fileName) {
+  const book = await loadBook(fileName);
+
+  // Set book title text
+  const title = document.getElementById("bookTitle");
+  title.innerText = "Book of " + book.book;
+
+  currentBook = book;
+
+  // Clear existing chapters
+  chapterSelector.textContent = "";
+
+  // Populate chapters
+  for (let i = 0; i < book.chapters.length; i++) {
+    const chapter = book.chapters[i];
+
+    const optionChapt = document.createElement("option");
+
+    optionChapt.innerText = chapter.chapter;
+    optionChapt.value = i;
+
+    chapterSelector.appendChild(optionChapt);
+  }
+}
+
+for (let i = 0; i < books.length; i++) {
+  const book = books[i];
+
+  const optionElem = document.createElement("option");
+
+  optionElem.innerText = book.title;
+  optionElem.value = book.filename;
+
+  bookSelector.appendChild(optionElem);
+}
+
+bookSelector.onchange = function () {
+  const filename = bookSelector.value;
+
+  selectBookByFileName(filename);
+};
+
+chapterSelector.onchange = function () {
+  if (currentBook == null) {
+    return;
+  }
+
+  const container0 = document.getElementById("verseContainer0");
+  const container1 = document.getElementById("verseContainer1");
+
+  const chapter = currentBook.chapters[chapterSelector.value];
+  const verses = chapter.verses;
+
+  function createElementForVerse(verse) {
+    const elem = document.createElement("p");
+    elem.classList.add("p-1");
+    elem.innerText = verse.verse + ". " + verse.text;
+
+    return elem;
+  }
+
+  for (const verse of verses) {
+    // Append to both containers
+    container0.appendChild(createElementForVerse(verse));
+    container1.appendChild(createElementForVerse(verse));
+  }
+};
 
 // Loads parallel text
-loadParallel(books[41]).then(function (book) {
-  console.log(book)
 
-  const container = document.getElementById('verseContainer1');
+// loadParallel(books[41]).then(function (book) {
+//   console.log(book);
 
-  const verses = book.chapters[0].verses;
-  for (const verse of verses) {
-    const elem = document.createElement('p');
-    elem.classList.add('p-1');
-    elem.innerText = verse.verse + '. ' + verse.text;
-    container.appendChild(elem);
-  }
+//   const container = document.getElementById("verseContainer1");
 
-  document.getElementById('BookTitleNumber').innerText = '';
-})
-
-
-
-
-
-
-
-
-// OLD, THIS IS FROM PARALLEL BRANCH
-
-// document.getElementById('parallelBtn').addEventListener('click', function() {
-//   let scriptureBlock = document.getElementById('parallelToggle');
-//   let mainScripture = document.getElementById('mainScripture');
-//   let parallelScripture = document.getElementById('parallelScripture')
-
-//   if (scriptureBlock.classList.contains('grid-cols-2')) {
-//     scriptureBlock.classList.remove('grid-cols-2');
-//   } else {
-//     scriptureBlock.classList.add('grid-cols-2');
-//   };
-
-//   if(parallelScripture.classList.contains('hidden')) {
-//     parallelScripture.classList.remove('hidden');
-//   } else {
-//     parallelScripture.classList.add('hidden');
+//   const verses = book.chapters[0].verses;
+//   for (const verse of verses) {
+//     const elem = document.createElement("p");
+//     elem.classList.add("p-1");
+//     elem.innerText = verse.verse + ". " + verse.text;
+//     container.appendChild(elem);
 //   }
-//   // FIX THIS ASAP
-//   if(mainScripture.classList.contains('xl:mx-52', 'lg:mx-40', 'md:mx-28', 'sm:mx-12')) {
-//     mainScripture.classList.add('xl:mr-0', 'lg:mr-0', 'md:mr-0', 'sm:mr-0');
-//   } else if (mainScripture.classList.contains('xl:mx-52', 'lg:mx-40', 'md:mx-28', 'sm:mx-12')) {
-//     mainScripture.classList.remove('xl:mr-0', 'lg:mr-0', 'md:mr-0', 'sm:mr-0')
-//   }
-// })
 
-// OLD, THIS IS FROM MASTER BRANCH
-
-// document.getElementById('parallelBtn').addEventListener('click', function() {
-//   let parallelBlock = document.getElementById('parallelBlock');
-//   let mainScripture = document.getElementById('mainScripture');
-
-//   if (parallelBlock.classList.contains('hidden')) {
-//     parallelBlock.classList.remove('hidden');
-//     parallelBlock.classList.add('grid', 'grid-cols-2');
-//   } else {
-//     parallelBlock.classList.add('hidden');
-//     parallelBlock.classList.remove('grid', 'grid-cols-2')
-//   }
+//   document.getElementById("BookTitleNumber").innerText = "";
 // });
