dce804de6cdc820df7936faa5e403ce62a5c7d1c
Omar <villalobosomar0@gmail.com>
2024-03-20 20:21:04 -0500

    deleting

--- server/server.js
+++ server/server.js
@@ -1,6 +1,4 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(3000);
