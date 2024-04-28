ecd66f5b25fb7516119301f2ad8fb151507094c2
Omar <villalobosomar0@gmail.com>
2024-04-28 15:46:03 -0500

    no change

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const session = require("express-session");
 const flash = require("express-flash");
 const passport = require("passport");
 const initializePassport = require("../config/passportConfig");
-const cfg = require("../config/sessionSecretConfig");
+import { cfg } from "../config/sessionSecretConfig.js";
 
 const PORT = process.env.PORT || 4000;
 
