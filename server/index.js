93e00d925c26cc7c0679970601d0ab92dab5d007
Omar <villalobosomar0@gmail.com>
2024-04-28 20:15:01 -0500

    .

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
 	session({
-		secret: process.env.SESSION_SECRET,
+		secret: "key",
 		resave: false,
 		saveUninitialized: false,
 	})
