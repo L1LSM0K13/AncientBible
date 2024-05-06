682577e09e699900a6a07f9d6c509ad449969e54
Omar <villalobosomar0@gmail.com>
2024-05-06 12:45:46 -0500

    added a post css processor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	mode: "jit",
-	content: ["./public/views/*.ejs", "scripts/*.js"],
+	content: ["./public/**/*.{ejs,js,html}"],
 	darkMode: "class",
 	theme: {
 		fontFamily: {
