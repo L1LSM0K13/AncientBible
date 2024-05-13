425413e4e8a7fdbdd442027e6788926d53512283
Omar <villalobosomar0@gmail.com>
2024-05-13 18:41:31 -0500

    .

--- server/index.js
+++ server/index.js
@@ -55,33 +55,33 @@ app.get("/users/login", checkAuthenticated, (req, res) => {
 	res.render("../public/views/login", { loggedIn: false });
 });
 
-// app.get("/users/bible", (req, res) => {
-// 	if (req.isAuthenticated()) {
-// 		pool.query(`SELECT * FROM englishbible`, (err, results) => {
-// 			if (err) {
-// 				return console.error("Error running query", err);
-// 			}
-// 			res.render("../public/views/scripture", {
-// 				books: results.rows,
-// 				loggedIn: true,
-// 			});
-// 		});
-// 	} else {
-// 		pool.query(
-// 			`SELECT * FROM englishbible WHERE (book, chapter_number) = ('John', 1)`,
-// 			(err, results) => {
-// 				if (err) {
-// 					return console.error("Error running query", err);
-// 				}
-// 				res.render("../public/views/scripture", {
-// 					books: results.rows,
-// 					loggedIn: false,
-// 				});
-// 			}
-// 		);
-// 	}
-// });
-bibleFunction();
+app.get("/users/bible", (req, res) => {
+	if (req.isAuthenticated()) {
+		pool.query(`SELECT * FROM englishbible`, (err, results) => {
+			if (err) {
+				return console.error("Error running query", err);
+			}
+			res.render("../public/views/scripture", {
+				books: results.rows,
+				loggedIn: true,
+			});
+		});
+	} else {
+		pool.query(
+			`SELECT * FROM englishbible WHERE (book, chapter_number) = ('John', 1)`,
+			(err, results) => {
+				if (err) {
+					return console.error("Error running query", err);
+				}
+				res.render("../public/views/scripture", {
+					books: results.rows,
+					loggedIn: false,
+				});
+			}
+		);
+	}
+});
+// bibleFunction();
 
 app.get("/users/fathers", (req, res) => {
 	if (req.isAuthenticated()) {
