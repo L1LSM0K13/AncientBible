6468e30d11e14b161d36bf390c6a21f9addb701e
Omar <villalobosomar0@gmail.com>
2024-05-13 18:37:30 -0500

    testing exporting the bible function

---
+++ server/bibleFunctionNEW.js
@@ -0,0 +1,30 @@
+function bibleFunction() {
+	app.get("/users/bible", (req, res) => {
+		if (req.isAuthenticated()) {
+			pool.query(`SELECT * FROM englishbible`, (err, results) => {
+				if (err) {
+					return console.error("Error running query", err);
+				}
+				res.render("../public/views/scripture", {
+					books: results.rows,
+					loggedIn: true,
+				});
+			});
+		} else {
+			pool.query(
+				`SELECT * FROM englishbible WHERE (book, chapter_number) = ('John', 1)`,
+				(err, results) => {
+					if (err) {
+						return console.error("Error running query", err);
+					}
+					res.render("../public/views/scripture", {
+						books: results.rows,
+						loggedIn: false,
+					});
+				}
+			);
+		}
+	});
+}
+
+export { bibleFunction };
