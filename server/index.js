256cb2b3cae30a4d5980d5b746ce58b7a66cbb74
Omar <villalobosomar0@gmail.com>
2024-05-13 20:17:01 -0500

    .

--- server/index.js
+++ server/index.js
@@ -24,9 +24,6 @@ initializePassport(passport);
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../public")));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
 app.use(
 	session({
 		secret: process.env.SESSION_SECRET,
@@ -34,6 +31,9 @@ app.use(
 		saveUninitialized: false,
 	})
 );
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.get("/", (req, res) => {
 	if (req.isAuthenticated()) {
@@ -67,66 +67,6 @@ app.get("/users/logout", (req, res) => {
 	req.flash("success_msg", "You have logged out");
 	res.redirect("/");
 });
-
-// app.post("/users/register", async (req, res) => {
-// 	let { name, email, password, password2 } = req.body;
-
-// 	let errors = [];
-
-// 	if (!name || !email || !password || !password) {
-// 		errors.push({ message: "Please enter all fields." });
-// 	}
-
-// 	if (password.length < 8) {
-// 		errors.push({ message: "password should be at least 8 characters long." });
-// 	}
-
-// 	if (password !== password2) {
-// 		errors.push({ message: "passwords must match." });
-// 	}
-
-// 	if (errors.length > 0) {
-// 		res.render("../public/views/register", { errors });
-// 	} else {
-// 		// Form validation has passed
-
-// 		let hashedPassword = await bcrypt.hash(password, 10);
-
-// 		pool.query(
-// 			`SELECT * FROM users
-//       WHERE email = $1`,
-// 			[email],
-// 			(err, results) => {
-// 				if (err) {
-// 					throw err;
-// 				}
-
-// 				if (results.rows.length > 0) {
-// 					errors.push({ message: "User with this email already exists" });
-// 					res.render("../public/views/register", { errors });
-// 				} else {
-// 					pool.query(
-// 						`INSERT INTO users (name, email, password)
-//             VALUES ($1, $2, $3)
-//             RETURNING id, password`,
-// 						[name, email, hashedPassword],
-// 						(err, results) => {
-// 							if (err) {
-// 								throw err;
-// 							}
-// 							console.log(results.rows);
-// 							req.flash(
-// 								"success_msg",
-// 								`You are now registered as ${name}. Please log in.`
-// 							);
-// 							res.redirect("/users/login");
-// 						}
-// 					);
-// 				}
-// 			}
-// 		);
-// 	}
-// });
 
 app.post(
 	"/users/login",
