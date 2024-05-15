02b1b35ae4057b80f7f9ab282f98df93778d50e3
Omar <villalobosomar0@gmail.com>
2024-05-15 18:01:20 -0500

    error catching.

--- server/bibleQuery.js
+++ server/bibleQuery.js
@@ -6,43 +6,48 @@ const loggedIn = req.isAuthenticated();
 
 async function bibleQuery(app, pool) {
 	app.get("/users/bible", async (req, res) => {
-		if (loggedIn) {
-			await pool.query(bookText, (err, results) => {
-				if (err) {
-					return console.error("Error running query", err);
-				}
-				res.render("../public/views/scripture", {
-					books: results.rows,
-					loggedIn: true,
+		try {
+			if (loggedIn) {
+				await pool.query(bookText, (err, results) => {
+					if (err) {
+						return console.error("Error running query", err);
+					}
+					res.render("../public/views/scripture", {
+						books: results.rows,
+						loggedIn: true,
+					});
 				});
-			});
-		} else {
-			await pool.query(bookText, (err, results) => {
-				if (err) {
-					return console.error("Error running query", err);
-				}
-				res.render("../public/views/scripture", {
-					books: results.rows,
-					loggedIn: false,
+			} else {
+				await pool.query(bookText, (err, results) => {
+					if (err) {
+						return console.error("Error running query", err);
+					}
+					res.render("../public/views/scripture", {
+						books: results.rows,
+						loggedIn: false,
+					});
 				});
-			});
 
-			// await pool.query(bookChapters, (err, results) => {
-			// 	if (err) {
-			// 		return console.error("Error running query", err);
-			// 	}
-			// 	res.render("../public/views/scripture", {
-			// 		bookTitles: results.rows,
-			// 		loggedIn: false,
-			// 	});
-			// });
+				// await pool.query(bookChapters, (err, results) => {
+				// 	if (err) {
+				// 		return console.error("Error running query", err);
+				// 	}
+				// 	res.render("../public/views/scripture", {
+				// 		bookTitles: results.rows,
+				// 		loggedIn: false,
+				// 	});
+				// });
 
-			// const { rows } = await pool.query(bookTitleOptions);
-			// const bookTitles = rows.map((row) => row.book);
-			// res.render("../public/views/scripture", {
-			// 	bookTitles: bookTitles,
-			// 	loggedIn: false,
-			// });
+				// const { rows } = await pool.query(bookTitleOptions);
+				// const bookTitles = rows.map((row) => row.book);
+				// res.render("../public/views/scripture", {
+				// 	bookTitles: bookTitles,
+				// 	loggedIn: false,
+				// });
+			}
+		} catch {
+			console.error("Error fatchng bible data", error);
+			res.status(502).send("Internal Server Error");
 		}
 	});
 }
