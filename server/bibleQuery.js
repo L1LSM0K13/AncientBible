af31e7bf4055349c1ebd177449bfb0a8180a8c7a
Omar <villalobosomar0@gmail.com>
2024-05-14 19:14:19 -0500

    book title query attempt 1

--- server/bibleQuery.js
+++ server/bibleQuery.js
@@ -1,15 +1,18 @@
 function bibleQuery(app, pool) {
 	app.get("/users/bible", (req, res) => {
 		if (req.isAuthenticated()) {
-			pool.query(`SELECT * FROM englishbible`, (err, results) => {
-				if (err) {
-					return console.error("Error running query", err);
+			pool.query(
+				`SELECT * FROM englishbible WHERE (book, chapter_number) = ('John', 1)`,
+				(err, results) => {
+					if (err) {
+						return console.error("Error running query", err);
+					}
+					res.render("../public/views/scripture", {
+						books: results.rows,
+						loggedIn: true,
+					});
 				}
-				res.render("../public/views/scripture", {
-					books: results.rows,
-					loggedIn: true,
-				});
-			});
+			);
 		} else {
 			pool.query(
 				`SELECT * FROM englishbible WHERE (book, chapter_number) = ('John', 1)`,
@@ -19,6 +22,19 @@ function bibleQuery(app, pool) {
 					}
 					res.render("../public/views/scripture", {
 						books: results.rows,
+						loggedIn: false,
+					});
+				}
+			);
+
+			pool.query(
+				`SELECT book FROM englishbible WHERE (chapter_number, verse_number) = (1,1)`,
+				(err, results) => {
+					if (err) {
+						return console.error("Error running query", err);
+					}
+					res.render("../public/views/scripture", {
+						bookTitles: results.rows,
 						loggedIn: false,
 					});
 				}
