513e99d1983f636a01858e53418f7747c0bc405b
Omar <villalobosomar0@gmail.com>
2024-05-15 17:55:21 -0500

    cleaning code

--- server/bibleQuery.js
+++ server/bibleQuery.js
@@ -1,71 +1,48 @@
+const bookChapters = `SELECT chapter_number FROM englishbible WHERE (book, verse_number) = ($1, 1)`;
+const bookTitleOptions = `SELECT book FROM englishbible WHERE (chapter_number, verse_number) = (1,1)`;
+const bookText = `SELECT * FROM englishbible WHERE (book, chapter_number) = ($1, 1)`;
+
+const loggedIn = req.isAuthenticated();
+
 async function bibleQuery(app, pool) {
 	app.get("/users/bible", async (req, res) => {
-		if (req.isAuthenticated()) {
-			await pool.query(
-				`SELECT * FROM englishbible WHERE (book, chapter_number) = ('John', 1)`,
-				(err, results) => {
-					if (err) {
-						return console.error("Error running query", err);
-					}
-					res.render("../public/views/scripture", {
-						books: results.rows,
-						loggedIn: true,
-					});
+		if (loggedIn) {
+			await pool.query(bookText, (err, results) => {
+				if (err) {
+					return console.error("Error running query", err);
 				}
-			);
-		} else {
-			// await pool.query(
-			// 	`SELECT * FROM englishbible WHERE (book, chapter_number) = ('John', 1)`,
-			// 	(err, results) => {
-			// 		if (err) {
-			// 			return console.error("Error running query", err);
-			// 		}
-			// 		res.render("../public/views/scripture", {
-			// 			books: results.rows,
-			// 			loggedIn: false,
-			// 		});
-			// 	}
-			// );
-
-			// await pool.query(
-			// 	`SELECT book FROM englishbible WHERE (chapter_number, verse_number) = (1,1)`,
-			// 	(err, results) => {
-			// 		if (err) {
-			// 			return console.error("Error running query", err);
-			// 		}
-			// 		res.render("../public/views/scripture", {
-			// 			bookTitles: results.rows,
-			// 			loggedIn: false,
-			// 		});
-			// 	}
-			// );
-
-			try {
-				const { rows } = await pool.query(
-					`SELECT book FROM englishbible WHERE (chapter_number, verse_number) = (1,1)`
-				);
-				const bookTitles = rows.map((row) => row.book); // Extracting just the book titles from the rows
 				res.render("../public/views/scripture", {
-					bookTitles: bookTitles,
+					books: results.rows,
+					loggedIn: true,
+				});
+			});
+		} else {
+			await pool.query(bookText, (err, results) => {
+				if (err) {
+					return console.error("Error running query", err);
+				}
+				res.render("../public/views/scripture", {
+					books: results.rows,
 					loggedIn: false,
 				});
-			} catch (err) {
-				console.error("Error running query", err);
-				res.status(500).send("Internal Server Error");
-			}
+			});
 
-			// await pool.query(
-			// 	`SELECT chapter_number FROM englishbible WHERE (book, verse_number) = ('Genesis', 1)`
-			// ),
-			// 	(err, results) => {
-			// 		if (err) {
-			// 			return console.error("Error running query", err);
-			// 		}
-			// 		res.render("../public/views/scripture.ejs", {
-			// 			chapterNumbers: results.rows,
-			// 			loggedIn: false,
-			// 		});
-			// 	};
+			// await pool.query(bookChapters, (err, results) => {
+			// 	if (err) {
+			// 		return console.error("Error running query", err);
+			// 	}
+			// 	res.render("../public/views/scripture", {
+			// 		bookTitles: results.rows,
+			// 		loggedIn: false,
+			// 	});
+			// });
+
+			// const { rows } = await pool.query(bookTitleOptions);
+			// const bookTitles = rows.map((row) => row.book);
+			// res.render("../public/views/scripture", {
+			// 	bookTitles: bookTitles,
+			// 	loggedIn: false,
+			// });
 		}
 	});
 }
