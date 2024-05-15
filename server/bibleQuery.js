a075697c1cdb97a9a244798f63a6d63dad6c59f5
Omar <villalobosomar0@gmail.com>
2024-05-15 18:11:41 -0500

    .

--- server/bibleQuery.js
+++ server/bibleQuery.js
@@ -1,11 +1,12 @@
 async function bibleQuery(app, pool) {
 	// const bookChapters = `SELECT chapter_number FROM englishbible WHERE (book, verse_number) = ($1, 1)`;
 	// const bookTitleOptions = `SELECT book FROM englishbible WHERE (chapter_number, verse_number) = (1,1)`;
-	const bookText = `SELECT * FROM englishbible WHERE (book, chapter_number) = ('John', 1)`;
-
-	const isAuth = req.isAuthenticated();
 
 	app.get("/users/bible", async (req, res) => {
+		const bookText = `SELECT * FROM englishbible WHERE (book, chapter_number) = ('John', 1)`;
+
+		const isAuth = req.isAuthenticated();
+
 		if (isAuth) {
 			await pool.query(bookText, (err, results) => {
 				if (err) {
