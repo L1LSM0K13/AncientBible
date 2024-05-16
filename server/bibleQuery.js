012e4682fa45d0287fe4e589a7258272e67fdaa0
Omar <villalobosomar0@gmail.com>
2024-05-15 20:25:19 -0500

    testing red lettering

--- server/bibleQuery.js
+++ server/bibleQuery.js
@@ -1,38 +1,50 @@
+// async function bibleQuery(app, pool) {
+// 	app.get("/users/bible", async (req, res) => {
+// 		const bookText = `SELECT * FROM englishbible WHERE (book, chapter_number) = ($1, 1)`;
+// 		const bookChapters = `SELECT chapter_number FROM englishbible WHERE (book, verse_number) = ($1, 1)`;
+// 		const bookTitleOptions = `SELECT book FROM englishbible WHERE (chapter_number, verse_number) = (1,1)`;
+
+// 		const isAuth = req.isAuthenticated();
+
+// 		if (isAuth) {
+// 			const [bookTitleOptionsRes, bookChaptersRes, bookTextRes] =
+// 				await Promise.all([
+// 					pool.query(bookTitleOptions),
+// 					pool.query(bookChapters),
+// 					pool.query(bookText),
+// 				]);
+
+// 			res.render("../public/views/scripture", {
+// 				bookText: bookTextRes.rows,
+// 				bookChapters: bookChaptersRes.rows.map((row) => row.chapter_number),
+// 				bookTitleOptions: bookTitleOptionsRes.rows.map((row) => row.book),
+// 				loggedIn: true,
+// 			});
+// 		} else {
+// 			const [bookTitleOptionsRes, bookChaptersRes, bookTextRes] =
+// 				await Promise.all([
+// 					pool.query(bookTitleOptions),
+// 					pool.query(bookChapters),
+// 					pool.query(bookText),
+// 				]);
+
+// 			res.render("../public/views/scripture", {
+// 				bookText: bookTextRes.rows,
+// 				bookChapters: bookChaptersRes.rows.map((row) => row.chapter_number),
+// 				bookTitleOptions: bookTitleOptionsRes.rows.map((row) => row.book),
+// 				loggedIn: false,
+// 			});
+// 		}
+// 	});
+// }
+
 async function bibleQuery(app, pool) {
 	app.get("/users/bible", async (req, res) => {
-		const { bookName, chapterNumber } = req.query;
-		const bookText = `SELECT * FROM englishbible WHERE (book, chapter_number) = ($1, 1)`;
-		const bookChapters = `SELECT chapter_number FROM englishbible WHERE (book, verse_number) = ($1, 1)`;
-		const bookTitleOptions = `SELECT book FROM englishbible WHERE (chapter_number, verse_number) = (1,1)`;
+		if (!req.isAuthenticated()) {
+			pool.query(`SELECT * FROM englishbible WHERE book = 'John'`);
 
-		const isAuth = req.isAuthenticated();
-
-		if (isAuth) {
-			const [bookTextRes, bookChaptersRes, bookTitleOptionsRes] =
-				await Promise.all([
-					pool.query(bookText, [bookName, chapterNumber]),
-					pool.query(bookChapters, [bookName]),
-					pool.query(bookTitleOptions),
-				]);
-
-			res.render("../public/views/scripture", {
-				bookText: bookTextRes.rows,
-				bookChapters: bookChaptersRes.rows.map((row) => row.chapter_number),
-				bookTitleOptions: bookTitleOptionsRes.rows.map((row) => row.book),
-				loggedIn: true,
-			});
-		} else {
-			const [bookTextRes, bookChaptersRes, bookTitleOptionsRes] =
-				await Promise.all([
-					pool.query(bookText, [bookName]),
-					pool.query(bookChapters, [bookName]),
-					pool.query(bookTitleOptions),
-				]);
-
-			res.render("../public/views/scripture", {
-				bookText: bookTextRes.rows,
-				bookChapters: bookChaptersRes.rows.map((row) => row.chapter_number),
-				bookTitleOptions: bookTitleOptionsRes.rows.map((row) => row.book),
+			res.render("../public/views/scripture.ejs", {
+				bookText: response.rows,
 				loggedIn: false,
 			});
 		}
