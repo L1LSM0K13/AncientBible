0b1a8e3f701e3a2e5ff35e65c22a17c59c6d60f9
Omar <villalobosomar0@gmail.com>
2024-05-19 18:53:26 -0500

    2nd attempt

--- server/fathersQuery.js
+++ server/fathersQuery.js
@@ -3,7 +3,7 @@ async function fathersQuery(app, pool) {
 		const defaultBook = req.query.book || "I Clement";
 		const defaultChapter = req.query.chapter || 1;
 
-		const bookTitleOptions = `SELECT DISTINCT book FROM fathersandwritings ORDER BY id`;
+		const bookTitleOptions = `SELECT book FROM fathersandwritings ORDER BY id`;
 		const bookChapters = `SELECT DISTINCT chapter_number FROM fathersandwritings WHERE book = $1 ORDER BY chapter_number`;
 		const bookText = `SELECT * FROM fathersandwritings WHERE book = $1 AND chapter_number = $2 ORDER BY id`;
 
