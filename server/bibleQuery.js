63b935c7c52479f9749b0d2d3bc06739efa972aa
Omar <villalobosomar0@gmail.com>
2024-05-20 18:35:11 -0500

    3rd attempt

--- server/bibleQuery.js
+++ server/bibleQuery.js
@@ -3,16 +3,16 @@ async function bibleQuery(app, pool) {
 		const defaultBook = req.query.book || "John";
 		let defaultChapter = req.query.chapter || 1;
 
-		const bookTitleOptions = `SELECT DISTINCT book, book_order FROM englishbible ORDER BY book_order;`;
-		const bookChapters = `SELECT DISTINCT chapter_number FROM englishbible WHERE book = $1 ORDER BY chapter_number`;
+		const bookTitleOptionsQuery = `SELECT DISTINCT book, book_order FROM englishbible ORDER BY book_order;`;
+		const bookChaptersQuery = `SELECT DISTINCT chapter_number FROM englishbible WHERE book = $1 ORDER BY chapter_number`;
 		const bookText = `SELECT * FROM englishbible WHERE book = $1 AND chapter_number = $2 ORDER BY verse_number`;
 
 		const isAuth = req.isAuthenticated();
 
 		if (isAuth) {
 			const [bookTitleOptionsRes, bookChaptersRes] = await Promise.all([
-				pool.query(bookTitleOptions),
-				pool.query(bookChapters, [defaultBook]),
+				pool.query(bookTitleOptionsQuery),
+				pool.query(bookChaptersQuery, [defaultBook]),
 			]);
 			const bookChapters = bookChaptersRes.rows.map(
 				(row) => row.chapter_number
@@ -26,14 +26,14 @@ async function bibleQuery(app, pool) {
 				loggedIn: true,
 				bookText: bookTextRes.rows,
 				bookChapters: bookChapters,
-				bookTitleOptions: bookTitleOptionsRes.rows.map((row) => row.book),
+				bookTitleOptionsQuery: bookTitleOptionsRes.rows.map((row) => row.book),
 				selectedBook: defaultBook,
 				selectedChapter: defaultChapter,
 			});
 		} else {
 			const [bookTitleOptionsRes, bookChaptersRes] = await Promise.all([
-				pool.query(bookTitleOptions),
-				pool.query(bookChapters, [defaultBook]),
+				pool.query(bookTitleOptionsQuery),
+				pool.query(bookChaptersQuery, [defaultBook]),
 			]);
 			const bookChapters = bookChaptersRes.rows.map(
 				(row) => row.chapter_number
@@ -47,7 +47,7 @@ async function bibleQuery(app, pool) {
 				loggedIn: false,
 				bookText: bookTextRes.rows,
 				bookChapters: bookChapters,
-				bookTitleOptions: bookTitleOptionsRes.rows.map((row) => row.book),
+				bookTitleOptionsQuery: bookTitleOptionsRes.rows.map((row) => row.book),
 				selectedBook: defaultBook,
 				selectedChapter: defaultChapter,
 			});
