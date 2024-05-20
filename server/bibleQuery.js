a272b6c35310d9ce3789b2efbde4d4a2e5b4e4e5
Omar <villalobosomar0@gmail.com>
2024-05-20 18:29:08 -0500

    2nd attempt

--- server/bibleQuery.js
+++ server/bibleQuery.js
@@ -1,7 +1,7 @@
 async function bibleQuery(app, pool) {
 	app.get("/users/bible", async (req, res) => {
 		const defaultBook = req.query.book || "John";
-		const defaultChapter = req.query.chapter || 1;
+		let defaultChapter = req.query.chapter || 1;
 
 		const bookTitleOptions = `SELECT DISTINCT book, book_order FROM englishbible ORDER BY book_order;`;
 		const bookChapters = `SELECT DISTINCT chapter_number FROM englishbible WHERE book = $1 ORDER BY chapter_number`;
@@ -10,33 +10,43 @@ async function bibleQuery(app, pool) {
 		const isAuth = req.isAuthenticated();
 
 		if (isAuth) {
-			const [bookTitleOptionsRes, bookChaptersRes, bookTextRes] =
-				await Promise.all([
-					pool.query(bookTitleOptions),
-					pool.query(bookChapters, [defaultBook]),
-					pool.query(bookText, [defaultBook, defaultChapter]),
-				]);
+			const [bookTitleOptionsRes, bookChaptersRes] = await Promise.all([
+				pool.query(bookTitleOptions),
+				pool.query(bookChapters, [defaultBook]),
+			]);
+			const bookChapters = bookChaptersRes.rows.map(
+				(row) => row.chapter_number
+			);
+			if (!req.query.chapter && bookChapters.length > 0) {
+				defaultChapter = bookChapters[0];
+			}
+			const bookTextRes = pool.query(bookText, [defaultBook, defaultChapter]);
 
 			res.render("../public/views/scripture", {
 				loggedIn: true,
 				bookText: bookTextRes.rows,
-				bookChapters: bookChaptersRes.rows.map((row) => row.chapter_number),
+				bookChapters: bookChapters,
 				bookTitleOptions: bookTitleOptionsRes.rows.map((row) => row.book),
 				selectedBook: defaultBook,
 				selectedChapter: defaultChapter,
 			});
 		} else {
-			const [bookTitleOptionsRes, bookChaptersRes, bookTextRes] =
-				await Promise.all([
-					pool.query(bookTitleOptions),
-					pool.query(bookChapters, [defaultBook]),
-					pool.query(bookText, [defaultBook, defaultChapter]),
-				]);
+			const [bookTitleOptionsRes, bookChaptersRes] = await Promise.all([
+				pool.query(bookTitleOptions),
+				pool.query(bookChapters, [defaultBook]),
+			]);
+			const bookChapters = bookChaptersRes.rows.map(
+				(row) => row.chapter_number
+			);
+			if (!req.query.chapter && bookChapters.length > 0) {
+				defaultChapter = bookChapters[0];
+			}
+			const bookTextRes = pool.query(bookText, [defaultBook, defaultChapter]);
 
 			res.render("../public/views/scripture", {
 				loggedIn: false,
 				bookText: bookTextRes.rows,
-				bookChapters: bookChaptersRes.rows.map((row) => row.chapter_number),
+				bookChapters: bookChapters,
 				bookTitleOptions: bookTitleOptionsRes.rows.map((row) => row.book),
 				selectedBook: defaultBook,
 				selectedChapter: defaultChapter,
