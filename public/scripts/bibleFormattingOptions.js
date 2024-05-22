3c2b7fdca97dd152a7c605af6a91a85f4dbe2b21
Omar <villalobosomar0@gmail.com>
2024-05-21 19:29:08 -0500

    5th attempt

--- public/scripts/bibleFormattingOptions.js
+++ public/scripts/bibleFormattingOptions.js
@@ -5,10 +5,20 @@ verseBtn.addEventListener("click", () => {
 	vContainer.classList.toggle("grid");
 });
 
-const redLetterBtn = document.getElementById("redLetterBtn");
-const redLettering = document.getElementById("redLettering");
+// const redLetterBtn = document.getElementById("redLetterBtn");
+// const redLettering = document.getElementById("redLettering");
 
-redLetterBtn.addEventListener("click", () => {
-	redLettering.classList.toggle("text-2xl");
-	console.log("worked");
+// redLetterBtn.addEventListener("click", () => {
+// 	redLettering.classList.toggle("text-2xl");
+// 	console.log("worked");
+// });
+
+document.getElementById("redLetterBtn").addEventListener("click", async () => {
+	const res = await fetch("/is_red", {
+		method: "POST",
+	});
+	const result = await res.json();
+	if (result.success) {
+		location.reload();
+	}
 });
--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ initializePassport(passport);
 
 // Middleware
 app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(
