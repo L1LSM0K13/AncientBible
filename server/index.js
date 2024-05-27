6401783f12991c71277f169e687523868072a9c7
Omar <villalobosomar0@gmail.com>
2024-05-27 12:58:05 -0500

    ,

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const { pool } = require("../config/dbConfig");
 const initializePassport = require("../config/passportConfig");
 const { bibleQuery } = require("./bibleQuery");
 const { fathersQuery } = require("./fathersQuery");
-const { register } = require("./register");
+// const { register } = require("./register");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -34,10 +34,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-app.get("/", checkNotAuthenticated(), (req, res) => {
+app.get("/", (req, res) => {
 	if (req.isAuthenticated()) {
 		res.render("../public/views/index", {
-			loggedIn: req.isAuthenticated(),
+			loggedIn: true,
 			user: req.isAuthenticated() ? req.user.name : null,
 		});
 	} else {
@@ -46,16 +46,84 @@ app.get("/", checkNotAuthenticated(), (req, res) => {
 });
 
 app.get("/users/register", checkAuthenticated, (req, res) => {
-	res.render("../public/views/register", { loggedIn: req.isAuthenticated() });
+	res.render("../public/views/register", { loggedIn: false });
 });
 
 app.get("/users/login", checkAuthenticated, (req, res) => {
-	res.render("../public/views/login", { loggedIn: req.isAuthenticated() });
+	res.render("../public/views/login", { loggedIn: false });
 });
 
 bibleQuery(app, pool);
 fathersQuery(app, pool);
-register(app, pool);
+// register(app, pool);
+
+/**
+ * @param {{ post: (arg0: string, arg1: (req: any, res: any) => Promise<void>) => void; }} app
+ * @param {{ query: (arg0: string, arg1: any[], arg2: { (err: any, results: any): void; (err: any, results: any): void; }) => void; }} pool
+ */
+// function register(app, pool) {
+app.post("/users/register", async (req, res) => {
+	let { name, email, password, password2 } = req.body;
+
+	let errors = [];
+
+	if (!name || !email || !password || !password) {
+		errors.push({ message: "Please enter all fields." });
+	}
+
+	if (password.length < 8) {
+		errors.push({
+			message: "password should be at least 8 characters long.",
+		});
+	}
+
+	if (password !== password2) {
+		errors.push({ message: "passwords must match." });
+	}
+
+	if (errors.length > 0) {
+		res.render("../public/views/register", { errors });
+	} else {
+		// Form validation has passed
+
+		let hashedPassword = await bcrypt.hash(password, 10);
+
+		pool.query(
+			`SELECT * FROM users
+        WHERE email = $1`,
+			[email],
+			(err, results) => {
+				if (err) {
+					throw err;
+				}
+
+				if (results.rows.length > 0) {
+					errors.push({ message: "User with this email already exists" });
+					res.render("../public/views/register", { errors });
+				} else {
+					pool.query(
+						`INSERT INTO users (name, email, password)
+              VALUES ($1, $2, $3)
+              RETURNING id, password`,
+						[name, email, hashedPassword],
+						(err, results) => {
+							if (err) {
+								throw err;
+							}
+							console.log(results.rows);
+							req.flash(
+								"success_msg",
+								`You are now registered as ${name}. Please log in.`
+							);
+							res.redirect("/users/login");
+						}
+					);
+				}
+			}
+		);
+	}
+});
+// }
 
 app.get("/users/logout", (req, res, next) => {
 	req.logout((err) => {
@@ -86,13 +154,6 @@ function checkAuthenticated(req, res, next) {
 		return res.redirect("/");
 	}
 	next();
-}
-
-function checkNotAuthenticated(req, res, next) {
-	if (req.isAuthenticated()) {
-		return next();
-	}
-	res.redirect("/users/login");
 }
 
 app.listen(PORT, () => {
--- server/register.js
+++ server/register.js
@@ -1,5 +1,3 @@
-const bcrypt = require("bcrypt");
-
 /**
  * @param {{ post: (arg0: string, arg1: (req: any, res: any) => Promise<void>) => void; }} app
  * @param {{ query: (arg0: string, arg1: any[], arg2: { (err: any, results: any): void; (err: any, results: any): void; }) => void; }} pool
