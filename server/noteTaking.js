b430f0e5e7fe26b9f3c35e96c37ba8be31d5e19c
Omar <villalobosomar0@gmail.com>
2024-06-03 20:25:08 -0500

    fixes

--- server/noteTaking.js
+++ server/noteTaking.js
@@ -21,7 +21,22 @@ async function takeNote(app, pool) {
 				[noteText]
 			);
 			console.table([result.rows]);
-			res.render("../public/views/scripture", { errors });
+
+			const renderedData = {
+				loggedIn: isAuth,
+				bookText: bookText,
+				bookChapters: chapters,
+				bookTitleOptions: bookTitles,
+				selectedBook: defaultBook,
+				selectedChapter: defaultChapter,
+				nextBook: nextBook,
+				nextChapter: nextChapter,
+				previousBook: previousBook,
+				previousChapter: previousChapter,
+				errors: errors,
+			};
+
+			res.render("../public/views/scripture", { renderedData });
 		}
 	});
 }
