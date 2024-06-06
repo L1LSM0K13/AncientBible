ea66e8f8ae908f7c9bfa6dce41b56491482ad966
Omar <villalobosomar0@gmail.com>
2024-06-05 20:27:13 -0500

    may revert changes

--- server/bibleQuery.js
+++ server/bibleQuery.js
@@ -1,3 +1,146 @@
+// async function bibleQuery(app, pool) {
+// 	app.get("/users/bible", async (req, res) => {
+// 		const { defaultRender } = require("./defaultValues");
+// 		const defaultBook = req.query.book || "John";
+// 		const defaultChapter = parseInt(req.query.chapter) || 1;
+
+// 		const bookTitleOptionsQuery = `SELECT DISTINCT book, book_order FROM bible_eng ORDER BY book_order;`;
+// 		const bookChaptersQuery = `SELECT DISTINCT chapter_number FROM bible_eng WHERE book = $1 ORDER BY chapter_number`;
+// 		const bookTextQuery = `SELECT * FROM bible_eng WHERE book = $1 AND chapter_number = $2 ORDER BY verse_number`;
+
+// 		const isAuth = req.isAuthenticated();
+
+// 		if (isAuth) {
+// 			const [bookTitleOptionRes, bookChaptersRes, bookTextRes] =
+// 				await Promise.all([
+// 					pool.query(bookTitleOptionsQuery),
+// 					pool.query(bookChaptersQuery, [defaultBook]),
+// 					pool.query(bookTextQuery, [defaultBook, defaultChapter]),
+// 				]);
+
+// 			const bookTitles = bookTitleOptionRes.rows.map((row) => row.book);
+// 			const chapters = bookChaptersRes.rows.map((row) => row.chapter_number);
+// 			const bookText = bookTextRes.rows;
+
+// 			let nextBook = defaultBook;
+// 			let nextChapter = defaultChapter + 1;
+// 			let previousBook = defaultBook;
+// 			let previousChapter = defaultChapter - 1;
+
+// 			// PAGINATION
+// 			if (nextChapter > chapters.length) {
+// 				const currentBookIndex = bookTitles.indexOf(defaultBook);
+
+// 				if (currentBookIndex < bookTitles.length - 1) {
+// 					nextBook = bookTitles[currentBookIndex + 1];
+// 					const nextChapterRes = await pool.query(bookChaptersQuery, [
+// 						nextBook,
+// 					]);
+// 					nextChapter = nextChapterRes.rows[0].chapter_number;
+// 				} else {
+// 					nextBook = bookTitles[0];
+// 					const nextChapterRes = await pool.query(bookChaptersQuery, [
+// 						nextBook,
+// 					]);
+// 					nextChapter = nextChapterRes.rows[0].chapter_number;
+// 				}
+// 			}
+// 			if (previousChapter < 1) {
+// 				const currentBookIndex = bookTitles.indexOf(defaultBook);
+
+// 				if (currentBookIndex > 0) {
+// 					previousBook = bookTitles[currentBookIndex - 1];
+// 				} else {
+// 					previousBook = bookTitles[bookTitles.length - 1];
+// 				}
+
+// 				const previousChapterRes = await pool.query(bookChaptersQuery, [
+// 					previousBook,
+// 				]);
+// 				previousChapter =
+// 					previousChapterRes.rows[previousChapterRes.rows.length - 1]
+// 						.chapter_number;
+// 			}
+
+// 			await defaultRender(req, res, true, "../public/views/scripture", {
+// 				bookText: bookText,
+// 				bookChapters: chapters,
+// 				bookTitleOptions: bookTitles,
+// 				selectedBook: defaultBook,
+// 				selectedChapter: defaultChapter,
+// 				nextBook: nextBook,
+// 				nextChapter: nextChapter,
+// 				previousBook: previousBook,
+// 				previousChapter: previousChapter,
+// 			});
+// 		} else {
+// 			const [bookTitleOptionRes, bookChaptersRes, bookTextRes] =
+// 				await Promise.all([
+// 					pool.query(bookTitleOptionsQuery),
+// 					pool.query(bookChaptersQuery, [defaultBook]),
+// 					pool.query(bookTextQuery, [defaultBook, defaultChapter]),
+// 				]);
+
+// 			const bookTitles = bookTitleOptionRes.rows.map((row) => row.book);
+// 			const chapters = bookChaptersRes.rows.map((row) => row.chapter_number);
+// 			const bookText = bookTextRes.rows;
+
+// 			let nextBook = defaultBook;
+// 			let nextChapter = defaultChapter + 1;
+// 			let previousBook = defaultBook;
+// 			let previousChapter = defaultChapter - 1;
+
+// 			// PAGINATION
+// 			if (nextChapter > chapters.length) {
+// 				const currentBookIndex = bookTitles.indexOf(defaultBook);
+
+// 				if (currentBookIndex < bookTitles.length - 1) {
+// 					nextBook = bookTitles[currentBookIndex + 1];
+// 					const nextChapterRes = await pool.query(bookChaptersQuery, [
+// 						nextBook,
+// 					]);
+// 					nextChapter = nextChapterRes.rows[0].chapter_number;
+// 				} else {
+// 					nextBook = bookTitles[0];
+// 					const nextChapterRes = await pool.query(bookChaptersQuery, [
+// 						nextBook,
+// 					]);
+// 					nextChapter = nextChapterRes.rows[0].chapter_number;
+// 				}
+// 			}
+// 			if (previousChapter < 1) {
+// 				const currentBookIndex = bookTitles.indexOf(defaultBook);
+
+// 				if (currentBookIndex > 0) {
+// 					previousBook = bookTitles[currentBookIndex - 1];
+// 				} else {
+// 					previousBook = bookTitles[bookTitles.length - 1];
+// 				}
+
+// 				const previousChapterRes = await pool.query(bookChaptersQuery, [
+// 					previousBook,
+// 				]);
+// 				previousChapter =
+// 					previousChapterRes.rows[previousChapterRes.rows.length - 1]
+// 						.chapter_number;
+// 			}
+
+// 			await defaultRender(req, res, false, "../public/views/scripture", {
+// 				bookText: bookText,
+// 				bookChapters: chapters,
+// 				bookTitleOptions: bookTitles,
+// 				selectedBook: defaultBook,
+// 				selectedChapter: defaultChapter,
+// 				nextBook: nextBook,
+// 				nextChapter: nextChapter,
+// 				previousBook: previousBook,
+// 				previousChapter: previousChapter,
+// 			});
+// 		}
+// 	});
+// }
+// module.exports = { bibleQuery };
+
 async function bibleQuery(app, pool) {
 	app.get("/users/bible", async (req, res) => {
 		const { defaultRender } = require("./defaultValues");
@@ -8,135 +151,64 @@ async function bibleQuery(app, pool) {
 		const bookChaptersQuery = `SELECT DISTINCT chapter_number FROM bible_eng WHERE book = $1 ORDER BY chapter_number`;
 		const bookTextQuery = `SELECT * FROM bible_eng WHERE book = $1 AND chapter_number = $2 ORDER BY verse_number`;
 
-		const isAuth = req.isAuthenticated();
+		const [bookTitleOptionRes, bookChaptersRes, bookTextRes] =
+			await Promise.all([
+				pool.query(bookTitleOptionsQuery),
+				pool.query(bookChaptersQuery, [defaultBook]),
+				pool.query(bookTextQuery, [defaultBook, defaultChapter]),
+			]);
 
-		if (isAuth) {
-			const [bookTitleOptionRes, bookChaptersRes, bookTextRes] =
-				await Promise.all([
-					pool.query(bookTitleOptionsQuery),
-					pool.query(bookChaptersQuery, [defaultBook]),
-					pool.query(bookTextQuery, [defaultBook, defaultChapter]),
-				]);
+		const bookTitles = bookTitleOptionRes.rows.map((row) => row.book);
+		const chapters = bookChaptersRes.rows.map((row) => row.chapter_number);
+		const bookText = bookTextRes.rows;
 
-			const bookTitles = bookTitleOptionRes.rows.map((row) => row.book);
-			const chapters = bookChaptersRes.rows.map((row) => row.chapter_number);
-			const bookText = bookTextRes.rows;
+		let nextBook = defaultBook;
+		let nextChapter = defaultChapter + 1;
+		let previousBook = defaultBook;
+		let previousChapter = defaultChapter - 1;
 
-			let nextBook = defaultBook;
-			let nextChapter = defaultChapter + 1;
-			let previousBook = defaultBook;
-			let previousChapter = defaultChapter - 1;
-
-			// PAGINATION
-			if (nextChapter > chapters.length) {
-				const currentBookIndex = bookTitles.indexOf(defaultBook);
-
-				if (currentBookIndex < bookTitles.length - 1) {
-					nextBook = bookTitles[currentBookIndex + 1];
-					const nextChapterRes = await pool.query(bookChaptersQuery, [
-						nextBook,
-					]);
-					nextChapter = nextChapterRes.rows[0].chapter_number;
-				} else {
-					nextBook = bookTitles[0];
-					const nextChapterRes = await pool.query(bookChaptersQuery, [
-						nextBook,
-					]);
-					nextChapter = nextChapterRes.rows[0].chapter_number;
-				}
-			}
-			if (previousChapter < 1) {
-				const currentBookIndex = bookTitles.indexOf(defaultBook);
-
-				if (currentBookIndex > 0) {
-					previousBook = bookTitles[currentBookIndex - 1];
-				} else {
-					previousBook = bookTitles[bookTitles.length - 1];
-				}
-
-				const previousChapterRes = await pool.query(bookChaptersQuery, [
-					previousBook,
-				]);
-				previousChapter =
-					previousChapterRes.rows[previousChapterRes.rows.length - 1]
-						.chapter_number;
-			}
-
-			await defaultRender(req, res, true, "../public/views/scripture", {
-				bookText: bookText,
-				bookChapters: chapters,
-				bookTitleOptions: bookTitles,
-				selectedBook: defaultBook,
-				selectedChapter: defaultChapter,
-				nextBook: nextBook,
-				nextChapter: nextChapter,
-				previousBook: previousBook,
-				previousChapter: previousChapter,
-			});
-		} else {
-			const [bookTitleOptionRes, bookChaptersRes, bookTextRes] =
-				await Promise.all([
-					pool.query(bookTitleOptionsQuery),
-					pool.query(bookChaptersQuery, [defaultBook]),
-					pool.query(bookTextQuery, [defaultBook, defaultChapter]),
-				]);
-
-			const bookTitles = bookTitleOptionRes.rows.map((row) => row.book);
-			const chapters = bookChaptersRes.rows.map((row) => row.chapter_number);
-			const bookText = bookTextRes.rows;
-
-			let nextBook = defaultBook;
-			let nextChapter = defaultChapter + 1;
-			let previousBook = defaultBook;
-			let previousChapter = defaultChapter - 1;
-
-			// PAGINATION
-			if (nextChapter > chapters.length) {
-				const currentBookIndex = bookTitles.indexOf(defaultBook);
-
-				if (currentBookIndex < bookTitles.length - 1) {
-					nextBook = bookTitles[currentBookIndex + 1];
-					const nextChapterRes = await pool.query(bookChaptersQuery, [
-						nextBook,
-					]);
-					nextChapter = nextChapterRes.rows[0].chapter_number;
-				} else {
-					nextBook = bookTitles[0];
-					const nextChapterRes = await pool.query(bookChaptersQuery, [
-						nextBook,
-					]);
-					nextChapter = nextChapterRes.rows[0].chapter_number;
-				}
-			}
-			if (previousChapter < 1) {
-				const currentBookIndex = bookTitles.indexOf(defaultBook);
-
-				if (currentBookIndex > 0) {
-					previousBook = bookTitles[currentBookIndex - 1];
-				} else {
-					previousBook = bookTitles[bookTitles.length - 1];
-				}
-
-				const previousChapterRes = await pool.query(bookChaptersQuery, [
-					previousBook,
-				]);
-				previousChapter =
-					previousChapterRes.rows[previousChapterRes.rows.length - 1]
-						.chapter_number;
-			}
-
-			await defaultRender(req, res, false, "../public/views/scripture", {
-				bookText: bookText,
-				bookChapters: chapters,
-				bookTitleOptions: bookTitles,
-				selectedBook: defaultBook,
-				selectedChapter: defaultChapter,
-				nextBook: nextBook,
-				nextChapter: nextChapter,
-				previousBook: previousBook,
-				previousChapter: previousChapter,
-			});
+		// PAGINATION
+		if (nextChapter > chapters.length) {
+			const currentBookIndex = bookTitles.indexOf(defaultBook);
+			nextBook = bookTitles[(currentBookIndex + 1) % bookTitles.length];
+			const nextChapterRes = await pool.query(bookChaptersQuery, [nextBook]);
+			nextChapter = nextChapterRes.rows[0].chapter_number;
 		}
+		if (previousChapter < 1) {
+			const currentBookIndex = bookTitles.indexOf(defaultBook);
+			previousBook =
+				bookTitles[
+					(currentBookIndex - 1 + bookTitles.length) % bookTitles.length
+				];
+			const previousChapterRes = await pool.query(bookChaptersQuery, [
+				previousBook,
+			]);
+			previousChapter =
+				previousChapterRes.rows[previousChapterRes.rows.length - 1]
+					.chapter_number;
+		}
+
+		const isAuth = req.isAuthenticated();
+		const renderData = {
+			bookText: bookText,
+			bookChapters: chapters,
+			bookTitleOptions: bookTitles,
+			selectedBook: defaultBook,
+			selectedChapter: defaultChapter,
+			nextBook: nextBook,
+			nextChapter: nextChapter,
+			previousBook: previousBook,
+			previousChapter: previousChapter,
+		};
+
+		await defaultRender(
+			req,
+			res,
+			isAuth,
+			"../public/views/scripture",
+			renderData
+		);
 	});
 }
+
 module.exports = { bibleQuery };
