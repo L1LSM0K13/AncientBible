81284a69c6c2aaf2c8e287aba412365e3604a55d
Omar <villalobosomar0@gmail.com>
2024-06-08 11:54:11 -0500

    more variable changes

--- server/noteTaking.js
+++ server/noteTaking.js
@@ -1,47 +1,31 @@
-// async function takeNote(app, pool) {
-// 	app.get("/users/bible", async (req, res) => {
-// 		const { defaultRender } = require("./defaultValues");
-
-// 		await defaultRender(req, res, true, "../public/views/scripture", {});
-// 	});
-// 	const { bibleQuery } = require("./bibleQuery");
-
-// 	app.post("/users/bible", async (req, res) => {
-// 		const { defaultRender } = require("./defaultValues");
-// 		let { noteText } = req.body;
-// 		let errors = [];
-
-// 		if (!noteText) {
-// 			errors.push({ message: "Note cannot be blank." });
-// 		}
-
-// 		if (errors.length > 0) {
-// 			await defaultRender(req, res, true, "../public/views/scripture", {
-// 				errors,
-// 			});
-// 		} else {
-// 			const result = await pool.query(
-// 				`INSERT INTO notes (text)
-// 				VALUES ($1)
-// 				RETURNING id, text`,
-// 				[noteText]
-// 			);
-// 			console.table([result.rows]);
-// 			try {
-// 				await bibleQuery(app, pool);
-// 			} catch (err) {
-// 				console.log(err);
-// 			}
-// 			await defaultRender(req, res, true, "../public/views/scripture", {});
-// 		}
-// 	});
-// }
-
 async function takeNote(app, pool) {
-	app.post("/users/bible", async (req, res) => {
-		console.log("POST /users/bible");
+	var bookText = bookText;
+	var bookChapters = chapters;
+	var bookTitleOptions = bookTitles;
+	var selectedBook = defaultBook;
+	var selectedChapter = defaultChapter;
+	var nextBook = nextBook;
+	var nextChapter = nextChapter;
+	var previousBook = previousBook;
+	var previousChapter = previousChapter;
+
+	app.get("/users/bible", async (req, res) => {
 		const { defaultRender } = require("./defaultValues");
-		const { bibleQuery } = require("./bibleQuery");
+		await defaultRender(req, res, true, "../public/views/scripture", {
+			bookText,
+			bookChapters,
+			bookTitleOptions,
+			selectedBook,
+			selectedChapter,
+			nextBook,
+			nextChapter,
+			previousBook,
+			previousChapter,
+		});
+	});
+
+	app.post("/users/bible", async (req, res) => {
+		const { defaultRender } = require("./defaultValues");
 		let { noteText } = req.body;
 		let errors = [];
 
@@ -51,30 +35,35 @@ async function takeNote(app, pool) {
 
 		if (errors.length > 0) {
 			await defaultRender(req, res, true, "../public/views/scripture", {
-				errors,
+				bookText,
+				bookChapters,
+				bookTitleOptions,
+				selectedBook,
+				selectedChapter,
+				nextBook,
+				nextChapter,
+				previousBook,
+				previousChapter,
 			});
 		} else {
-			try {
-				const result = await pool.query(
-					`INSERT INTO notes (text) VALUES ($1) RETURNING id, text`,
-					[noteText]
-				);
-				console.table(result.rows);
-
-				// Assuming bibleQuery is necessary and performs some critical action
-				await bibleQuery(app, pool);
-
-				// Render success response
-				await defaultRender(req, res, true, "../public/views/scripture", {
-					note: result.rows[0],
-				});
-			} catch (err) {
-				console.error("Database error:", err);
-				// Render error response
-				await defaultRender(req, res, true, "../public/views/scripture", {
-					errors: [{ message: "Database error." }],
-				});
-			}
+			const result = await pool.query(
+				`INSERT INTO notes (text)
+				VALUES ($1)
+				RETURNING id, text`,
+				[noteText]
+			);
+			console.table([result.rows]);
+			await defaultRender(req, res, true, "../public/views/scripture", {
+				bookText,
+				bookChapters,
+				bookTitleOptions,
+				selectedBook,
+				selectedChapter,
+				nextBook,
+				nextChapter,
+				previousBook,
+				previousChapter,
+			});
 		}
 	});
 }
