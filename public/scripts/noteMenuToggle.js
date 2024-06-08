0a371154f1317d49fbbafa4b4ed305f7a77d87a5
Omar <villalobosomar0@gmail.com>
2024-06-08 15:43:01 -0500

    note menu ui fixes

---
+++ public/scripts/noteMenuToggle.js
@@ -0,0 +1,7 @@
+const noteMenu = document.getElementById("createNoteMenu");
+
+const toggleNoteMenu = () => {
+	console.log("this worked");
+	noteMenu.classList.toggle("hidden");
+	noteMenu.classList.toggle("flex");
+};
