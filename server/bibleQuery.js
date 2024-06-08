8c7a57ffcfbfa65b31afeb40029a8e5df5dcd27e
Omar <villalobosomar0@gmail.com>
2024-06-07 20:19:31 -0500

    .

--- server/bibleQuery.js
+++ server/bibleQuery.js
@@ -59,21 +59,13 @@ async function bibleQuery(app, pool) {
 		};
 
 		if (isAuth) {
-			await defaultRender(
-				req,
-				res,
-				true,
-				"../public/views/scripture.ejs",
-				renderData
-			);
+			await defaultRender(req, res, true, "../public/views/scripture.ejs", {
+				renderData,
+			});
 		} else {
-			await defaultRender(
-				req,
-				res,
-				false,
-				"../public/views/scripture.ejs",
-				renderData
-			);
+			await defaultRender(req, res, false, "../public/views/scripture.ejs", {
+				renderData,
+			});
 		}
 	});
 }
