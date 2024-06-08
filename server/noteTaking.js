aa14ab21f012ba79d5cbd5496e44879600c83ea6
Omar <villalobosomar0@gmail.com>
2024-06-07 19:49:05 -0500

    .

--- server/noteTaking.js
+++ server/noteTaking.js
@@ -1,9 +1,21 @@
 async function takeNote(app, pool) {
 	app.get("/users/bible", async (req, res) => {
 		const { defaultRender } = require("./defaultValues");
+		const renderData = {
+			bookText: bookText,
+			bookChapters: chapters,
+			bookTitleOptions: bookTitles,
+			selectedBook: defaultBook,
+			selectedChapter: defaultChapter,
+			nextBook: nextBook,
+			nextChapter: nextChapter,
+			previousBook: previousBook,
+			previousChapter: previousChapter,
+		};
 
 		await defaultRender(req, res, true, "../public/views/scripture", {
 			errors: [],
+			renderData,
 		});
 	});
 
@@ -11,13 +23,26 @@ async function takeNote(app, pool) {
 		const { defaultRender } = require("./defaultValues");
 		let { noteText } = req.body;
 		let errors = [];
+		const renderData = {
+			bookText: bookText,
+			bookChapters: chapters,
+			bookTitleOptions: bookTitles,
+			selectedBook: defaultBook,
+			selectedChapter: defaultChapter,
+			nextBook: nextBook,
+			nextChapter: nextChapter,
+			previousBook: previousBook,
+			previousChapter: previousChapter,
+		};
 
 		if (!noteText) {
 			errors.push({ message: "Note cannot be blank." });
 		}
 
 		if (errors.length > 0) {
-			await defaultRender(req, res, true, "../public/views/scripture", {});
+			await defaultRender(req, res, true, "../public/views/scripture", {
+				renderData,
+			});
 		} else {
 			const result = await pool.query(
 				`INSERT INTO notes (text)
@@ -26,7 +51,9 @@ async function takeNote(app, pool) {
 				[noteText]
 			);
 			console.table([result.rows]);
-			await defaultRender(req, res, true, "../public/views/scripture", {});
+			await defaultRender(req, res, true, "../public/views/scripture", {
+				renderData,
+			});
 		}
 	});
 }
