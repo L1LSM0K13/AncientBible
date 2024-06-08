f26d81d26e7b3ef473bd227106ce82dac8594786
Omar <villalobosomar0@gmail.com>
2024-06-08 16:01:31 -0500

    .

--- public/scripts/noteMenuToggle.js
+++ public/scripts/noteMenuToggle.js
@@ -1,7 +1,16 @@
-const noteMenu = document.getElementById("createNoteMenu");
+// const noteMenu = document.getElementById("createNoteMenu");
 
-const toggleNoteMenu = () => {
-	console.log("this worked");
-	noteMenu.classList.toggle("hidden");
-	noteMenu.classList.toggle("flex");
+// const toggleNoteMenu = () => {
+// 	console.log("this worked");
+// 	noteMenu.classList.toggle("hidden");
+// 	noteMenu.classList.toggle("flex");
+// };
+
+const toggleNoteMenu = (noteMenuId) => {
+	const noteMenu = document.getElementById(noteMenuId);
+	if (noteMenu) {
+		console.log("this worked");
+		noteMenu.classList.toggle("hidden");
+		noteMenu.classList.toggle("flex");
+	}
 };
