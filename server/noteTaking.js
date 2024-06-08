f58490b6571c8d214904a294dc774e30703415a7
Omar <villalobosomar0@gmail.com>
2024-06-08 13:33:32 -0500

    removed the second app.get request on noteTaking.js

--- server/noteTaking.js
+++ server/noteTaking.js
@@ -1,8 +1,8 @@
 async function takeNote(app, pool) {
-	app.get("/users/bible", async (req, res) => {
-		const { defaultRender } = require("./defaultValues");
-		await defaultRender(req, res, true, "../public/views/scripture", {});
-	});
+	// app.get("/users/bible", async (req, res) => {
+	// 	const { defaultRender } = require("./defaultValues");
+	// 	await defaultRender(req, res, true, "../public/views/scripture", {});
+	// });
 
 	app.post("/users/bible", async (req, res) => {
 		const { defaultRender } = require("./defaultValues");
