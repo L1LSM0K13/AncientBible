9357ae2dad3c693e28d2691e48acadfdfe5ffe8c
Omar <villalobosomar0@gmail.com>
2024-06-11 18:32:46 -0500

    2nd attempt at note taking June 11

--- server/noteTaking.js
+++ server/noteTaking.js
@@ -1,13 +1,38 @@
 async function takeNote(app, pool) {
 	app.post("/users/bible", async (req, res) => {
+		fetch("/users/bible", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				noteText: noteText,
+				verse_id: verse_id,
+				fathers_id: fathers_id,
+			}),
+		})
+			.then((res) => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					throw new Error("Failed to save note");
+				}
+			})
+			.then((data) => {
+				console.log("Note Saved: ", data);
+			})
+			.catch((err) => {
+				console.error("Error:", err);
+			});
+
 		const { defaultRender } = require("./defaultValues");
 		const isAuth = req.isAuthenticated();
 		const user_id = req.user.id;
-		let { noteText } = req.body;
+		let { noteText, verse_id, fathers_id } = req.body;
 
 		const result = await pool.query(
-			`INSERT INTO user_notes (text, user_id) VALUES ($1, $2) RETURNING id, text, user_id`,
-			[noteText, user_id]
+			`INSERT INTO user_notes (text, user_id, verse_id, fathers_id) VALUES ($1, $2, $3, $4) RETURNING id, text, user_id, verse_id, fathers_id`,
+			[noteText, user_id, verse_id, fathers_id]
 		);
 		console.table(result.rows);
 
