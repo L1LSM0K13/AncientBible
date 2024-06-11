826c5926aafbab975e70368aafa8cd4f91304969
Omar <villalobosomar0@gmail.com>
2024-06-11 18:34:20 -0500

    initialization fixes

--- server/noteTaking.js
+++ server/noteTaking.js
@@ -1,5 +1,16 @@
 async function takeNote(app, pool) {
 	app.post("/users/bible", async (req, res) => {
+		const { defaultRender } = require("./defaultValues");
+		const isAuth = req.isAuthenticated();
+		const user_id = req.user.id;
+		let { noteText, verse_id, fathers_id } = req.body;
+
+		const result = await pool.query(
+			`INSERT INTO user_notes (text, user_id, verse_id, fathers_id) VALUES ($1, $2, $3, $4) RETURNING id, text, user_id, verse_id, fathers_id`,
+			[noteText, user_id, verse_id, fathers_id]
+		);
+		console.table(result.rows);
+
 		fetch("/users/bible", {
 			method: "POST",
 			headers: {
@@ -24,17 +35,6 @@ async function takeNote(app, pool) {
 			.catch((err) => {
 				console.error("Error:", err);
 			});
-
-		const { defaultRender } = require("./defaultValues");
-		const isAuth = req.isAuthenticated();
-		const user_id = req.user.id;
-		let { noteText, verse_id, fathers_id } = req.body;
-
-		const result = await pool.query(
-			`INSERT INTO user_notes (text, user_id, verse_id, fathers_id) VALUES ($1, $2, $3, $4) RETURNING id, text, user_id, verse_id, fathers_id`,
-			[noteText, user_id, verse_id, fathers_id]
-		);
-		console.table(result.rows);
 
 		if (isAuth) {
 			await defaultRender(req, res, true, "../public/views/scripture", {
