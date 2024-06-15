ee9b7a42cf3c55c8320d3f36e6695bd785d19770
Omar <villalobosomar0@gmail.com>
2024-06-15 09:47:33 -0500

    .

--- server/noteTaking.js
+++ server/noteTaking.js
@@ -3,13 +3,18 @@ async function takeNote(app, pool) {
 		const user_id = req.user.id;
 		let { noteText, verse_id, fathers_id } = req.body;
 
-		const result = await pool.query(
-			`INSERT INTO user_notes (text, user_id, verse_id, fathers_id) VALUES ($1, $2, $3, $4) RETURNING id, text, user_id, verse_id, fathers_id`,
-			[noteText, user_id, verse_id, fathers_id]
-		);
-		console.table(result.rows);
+		try {
+			const result = await pool.query(
+				`INSERT INTO user_notes (text, user_id, verse_id, fathers_id) VALUES ($1, $2, $3, $4) RETURNING id, text, user_id, verse_id, fathers_id`,
+				[noteText, user_id, verse_id, fathers_id]
+			);
+			console.table(result.rows);
 
-		res.redirect("/users/bible");
+			res.redirect("/users/bible");
+		} catch (err) {
+			console.log(err);
+			res.status(500).send("server error");
+		}
 	});
 }
 
