2db2026b4dab7cf069a2a39105f8e5c011ad2c73
Omar <villalobosomar0@gmail.com>
2024-06-14 19:09:30 -0500

    .

--- server/noteTaking.js
+++ server/noteTaking.js
@@ -3,8 +3,6 @@ async function takeNote(app, pool) {
 		const user_id = req.user.id;
 		let { noteText, verse_id, fathers_id } = req.body;
 
-		verse_id = parseInt(verse_id, 10);
-
 		const result = await pool.query(
 			`INSERT INTO user_notes (text, user_id, verse_id, fathers_id) VALUES ($1, $2, $3, $4) RETURNING id, text, user_id, verse_id, fathers_id`,
 			[noteText, user_id, verse_id, fathers_id]
