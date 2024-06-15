016d7c79b83c008a298e79b290b59b2f37953c08
Omar <villalobosomar0@gmail.com>
2024-06-15 15:33:07 -0500

    .

--- server/bibleQuery.js
+++ server/bibleQuery.js
@@ -4,7 +4,7 @@ async function bibleQuery(app, pool) {
 		const defaultBook = req.query.book || "John";
 		const defaultChapter = parseInt(req.query.chapter) || 1;
 
-		const notes = req.body;
+		const { notes } = req.body;
 
 		const bookTitleOptionsQuery = `SELECT DISTINCT book, book_order FROM bible_eng ORDER BY book_order;`;
 		const bookChaptersQuery = `SELECT DISTINCT chapter_number FROM bible_eng WHERE book = $1 ORDER BY chapter_number`;
