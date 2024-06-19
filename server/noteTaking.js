558a259bbcafde74db4e383b5411f664f613cb4d
Omar <villalobosomar0@gmail.com>
2024-06-18 19:14:26 -0500

    merged functions

--- server/noteTaking.js
+++ server/noteTaking.js
@@ -16,23 +16,13 @@ async function takeNote(app, pool) {
 			console.log(err);
 			res.status(500).send("server error");
 		}
-	});
-}
-
-async function deleteNote(app, pool) {
-	app.post("/users/bible", async (req, res) => {
-		const user_id = req.user.id;
-		let { verse_id, fathers_id } = req.body;
 
 		try {
 			const results = await pool.query(
 				`DELETE FROM user_notes WHERE (id, user_id, fathers_id) = ($1, $2, $3)`,
 				[user_id, verse_id, fathers_id]
 			);
-
 			console.table(results.rows);
-
-			res.redirect("/users/bible");
 		} catch (err) {
 			console.log(err);
 			res.status(500).send("Could not send request");
@@ -40,5 +30,12 @@ async function deleteNote(app, pool) {
 	});
 }
 
+// async function deleteNote(app, pool) {
+// 	app.post("/users/bible", async (req, res) => {
+// 		const user_id = req.user.id;
+// 		let { verse_id, fathers_id } = req.body;
+// 	});
+// }
+
 module.exports = { takeNote };
-module.exports = { deleteNote };
+// module.exports = { deleteNote };
