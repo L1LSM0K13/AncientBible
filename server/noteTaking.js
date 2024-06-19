58089ba272df972ada3a83db3c830aed3e65a4e0
Omar <villalobosomar0@gmail.com>
2024-06-18 19:44:32 -0500

    .

--- server/noteTaking.js
+++ server/noteTaking.js
@@ -1,5 +1,5 @@
 async function takeNote(app, pool) {
-	app.post("/users/bible", async (req, res) => {
+	app.post("/users/bible/insert", async (req, res) => {
 		const user_id = req.user.id;
 		let { noteText, verse_id, fathers_id } = req.body;
 
@@ -16,13 +16,20 @@ async function takeNote(app, pool) {
 			console.log(err);
 			res.status(500).send("server error");
 		}
+	});
+}
+
+async function deleteNote(app, pool) {
+	app.post("/users/bible/delete", async (req, res) => {
+		const user_id = req.user.id;
+		let { note_id, verse_id, fathers_id } = req.body;
 
 		try {
-			const deleteResults = await pool.query(
+			const results = await pool.query(
 				`DELETE FROM user_notes WHERE (id, user_id, verse_id, fathers_id) = ($1, $2, $3, $4) RETURNING id, text, user_id, verse_id, fathers_id`,
-				[results.rows[0].id, user_id, verse_id, fathers_id]
+				[note_id, user_id, verse_id, fathers_id]
 			);
-			console.table(deleteResults.rows);
+			console.table(results.rows);
 
 			res.redirect("/users/bible");
 		} catch (err) {
@@ -32,12 +39,5 @@ async function takeNote(app, pool) {
 	});
 }
 
-// async function deleteNote(app, pool) {
-// 	app.post("/users/bible", async (req, res) => {
-// 		const user_id = req.user.id;
-// 		let { verse_id, fathers_id } = req.body;
-// 	});
-// }
-
 module.exports = { takeNote };
-// module.exports = { deleteNote };
+module.exports = { deleteNote };
