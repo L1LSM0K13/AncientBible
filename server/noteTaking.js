bcf28046df0fcaf9026f8ed1017fb822dc872c10
Omar <villalobosomar0@gmail.com>
2024-06-18 19:57:35 -0500

    2nd attempt for deletion

--- server/noteTaking.js
+++ server/noteTaking.js
@@ -1,43 +1,60 @@
 async function takeNote(app, pool) {
-	app.post("/users/bible/insert", async (req, res) => {
+	app.post("/users/bible", async (req, res) => {
 		const user_id = req.user.id;
 		let { noteText, verse_id, fathers_id } = req.body;
 
-		try {
-			const results = await pool.query(
-				`INSERT INTO user_notes (text, user_id, verse_id, fathers_id) VALUES ($1, $2, $3, $4) RETURNING id, text, user_id, verse_id, fathers_id`,
-				[noteText, user_id, verse_id, fathers_id]
-			);
+		if (noteText) {
+			try {
+				const results = await pool.query(
+					`INSERT INTO user_notes (text, user_id, verse_id, fathers_id) VALUES ($1, $2, $3, $4) RETURNING id, text, user_id, verse_id, fathers_id`,
+					[noteText, user_id, verse_id, fathers_id]
+				);
 
-			console.table(results.rows);
+				console.table(results.rows);
 
-			res.redirect("/users/bible");
-		} catch (err) {
-			console.log(err);
-			res.status(500).send("server error");
+				res.redirect("/users/bible");
+			} catch (err) {
+				console.log(err);
+				res.status(500).send("server error");
+			}
+		}
+
+		if (note_id) {
+			try {
+				const results = await pool.query(
+					`DELETE FROM user_notes WHERE (id, user_id) = ($1, $2) RETURNING id, text, user_id, verse_id, fathers_id`,
+					[note_id, user_id]
+				);
+				console.table(results.rows);
+
+				res.redirect("/users/bible");
+			} catch (err) {
+				console.log(err);
+				res.status(500).send("Could not send request");
+			}
 		}
 	});
 }
 
-async function deleteNote(app, pool) {
-	app.post("/users/bible/delete", async (req, res) => {
-		const user_id = req.user.id;
-		let { note_id, verse_id, fathers_id } = req.body;
+// async function deleteNote(app, pool) {
+// 	app.post("/users/bible", async (req, res) => {
+// 		const user_id = req.user.id;
+// 		let { note_id, verse_id, fathers_id } = req.body;
 
-		try {
-			const results = await pool.query(
-				`DELETE FROM user_notes WHERE (id, user_id, verse_id, fathers_id) = ($1, $2, $3, $4) RETURNING id, text, user_id, verse_id, fathers_id`,
-				[note_id, user_id, verse_id, fathers_id]
-			);
-			console.table(results.rows);
+// 		try {
+// 			const results = await pool.query(
+// 				`DELETE FROM user_notes WHERE (id, user_id, verse_id, fathers_id) = ($1, $2, $3, $4) RETURNING id, text, user_id, verse_id, fathers_id`,
+// 				[note_id, user_id, verse_id, fathers_id]
+// 			);
+// 			console.table(results.rows);
 
-			res.redirect("/users/bible");
-		} catch (err) {
-			console.log(err);
-			res.status(500).send("Could not send request");
-		}
-	});
-}
+// 			res.redirect("/users/bible");
+// 		} catch (err) {
+// 			console.log(err);
+// 			res.status(500).send("Could not send request");
+// 		}
+// 	});
+// }
 
 module.exports = { takeNote };
-module.exports = { deleteNote };
+// module.exports = { deleteNote };
