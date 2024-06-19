5370fdefb550169f8ce10a103c27fb1201f5492a
Omar <villalobosomar0@gmail.com>
2024-06-18 20:08:59 -0500

    removed old code

--- server/noteTaking.js
+++ server/noteTaking.js
@@ -36,25 +36,4 @@ async function takeNote(app, pool) {
 	});
 }
 
-// async function deleteNote(app, pool) {
-// 	app.post("/users/bible", async (req, res) => {
-// 		const user_id = req.user.id;
-// 		let { note_id, verse_id, fathers_id } = req.body;
-
-// 		try {
-// 			const results = await pool.query(
-// 				`DELETE FROM user_notes WHERE (id, user_id, verse_id, fathers_id) = ($1, $2, $3, $4) RETURNING id, text, user_id, verse_id, fathers_id`,
-// 				[note_id, user_id, verse_id, fathers_id]
-// 			);
-// 			console.table(results.rows);
-
-// 			res.redirect("/users/bible");
-// 		} catch (err) {
-// 			console.log(err);
-// 			res.status(500).send("Could not send request");
-// 		}
-// 	});
-// }
-
 module.exports = { takeNote };
-// module.exports = { deleteNote };
