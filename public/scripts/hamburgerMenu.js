2ded33c2e2d89fce7ca693d2ef2a6737c9c4829b
Omar <omarmares@purelymail.com>
2024-07-29 18:48:50 -0500

    finished new navbar

--- public/scripts/hamburgerMenu.js
+++ public/scripts/hamburgerMenu.js
@@ -1,20 +1,11 @@
 const toggleBtn = document.getElementById('toggleBtn');
-const option1 = document.getElementById('option1');
-const option2 = document.getElementById('option2');
-const option3 = document.getElementById('option3');
-const option4 = document.getElementById('option4');
-const option5 = document.getElementById('option5');
-const option6 = document.getElementById('option6');
-const option7 = document.getElementById('option7');
-
-// TODO improve this ungodly code
+const menuOptions = document.querySelectorAll('.menuOption');
 
 toggleBtn.addEventListener('click', () => {
-    option1.classList.toggle('hidden');
-    option2.classList.toggle('hidden');
-    option3.classList.toggle('hidden');
-    option4.classList.toggle('hidden');
-    option5.classList.toggle('hidden');
-    option6.classList.toggle('hidden');
-    option7.classList.toggle('hidden');
+    menuOptions.forEach((option) => {
+        if (option) {
+            option.classList.toggle('hidden');
+        }
+    })
 })
+
