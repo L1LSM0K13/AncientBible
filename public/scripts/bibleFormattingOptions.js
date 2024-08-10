3c233e9c9ced051cf763682c47602e1aa436ffb2
= <omarmares@purelymail.com>
2024-08-09 20:47:57 -0500

    font size and type locally stored

--- public/scripts/bibleFormattingOptions.js
+++ public/scripts/bibleFormattingOptions.js
@@ -1,58 +1,89 @@
-const verseBtn = document.getElementById('verseBtn');
-const vContainer = document.getElementById('vContainer');
-const redLetterBtn = document.getElementById('redLetterBtn');
+const verseBtn = document.getElementById('verseBtn')
+const vContainer = document.getElementById('vContainer')
+const redLetterBtn = document.getElementById('redLetterBtn')
 
-let verseByVerse = false;
-let isRed = true;
+const fontSizeElem = document.getElementById('fontSize')
+const fontTypeElem = document.getElementById('fontType')
+
+let verseByVerse = false
+let isRed = true
 
 // Grid toggle
 verseBtn.addEventListener('click', () => {
-	verseByVerse = !verseByVerse;
-	vContainer.classList.toggle('grid', verseByVerse);
+	verseByVerse = !verseByVerse
+	vContainer.classList.toggle('grid', verseByVerse)
 
-	localStorage.setItem('verseByVerse', JSON.stringify(verseByVerse));
-});
+	localStorage.setItem('verseByVerse', JSON.stringify(verseByVerse))
+})
 
 function loadSavedVerseByVerse() {
-	const savedVerseByVerse = JSON.parse(localStorage.getItem('verseByVerse'));
+	const savedVerseByVerse = JSON.parse(localStorage.getItem('verseByVerse'))
 	if (savedVerseByVerse !== null) {
-		verseByVerse = savedVerseByVerse;
-		vContainer.classList.toggle('grid', verseByVerse);
+		verseByVerse = savedVerseByVerse
+		vContainer.classList.toggle('grid', verseByVerse)
 	}
 }
 loadSavedVerseByVerse()
 
 // Red lettering toggle
 redLetterBtn.addEventListener('click', () => {
-	isRed = !isRed;
-	const redLettering = document.querySelectorAll('#redLettering');
+	isRed = !isRed
+	const redLettering = document.querySelectorAll('#redLettering')
 
-	localStorage.setItem('is_red', JSON.stringify(isRed));
+	localStorage.setItem('is_red', JSON.stringify(isRed))
 
 	redLettering.forEach((redLettering) => {
 		if (isRed) {
-			redLettering.classList.remove('text-red-600', 'dark:text-red-500');
+			redLettering.classList.remove('text-red-600', 'dark:text-red-500')
 		} else {
-			redLettering.classList.add('text-red-600', 'dark:text-red-500');
+			redLettering.classList.add('text-red-600', 'dark:text-red-500')
 		}
 	})
 
 })
 
 function loadSavedRedLettering() {
-	const savedRedLettering = JSON.parse(localStorage.getItem('is_red'));
-	const redLettering = document.querySelectorAll('#redLettering');
+	const savedRedLettering = JSON.parse(localStorage.getItem('is_red'))
+	const redLettering = document.querySelectorAll('#redLettering')
 
 	if (savedRedLettering !== null) {
-		isRed = savedRedLettering;
+		isRed = savedRedLettering
 
 		redLettering.forEach((redLettering) => {
 			if (isRed) {
-				redLettering.classList.remove('text-red-600', 'dark:text-red-500');
+				redLettering.classList.remove('text-red-600', 'dark:text-red-500')
 			} else {
-				redLettering.classList.add('text-red-600', 'dark:text-red-500');
+				redLettering.classList.add('text-red-600', 'dark:text-red-500')
 			}
 		})
 	}
 }
 loadSavedRedLettering()
+
+
+// Font type and size
+fontSizeElem.addEventListener('change', () => {
+	let selectedFontSize = fontSizeElem.value
+	vContainer.style.fontSize = selectedFontSize
+
+	localStorage.setItem('fontSize', JSON.stringify(selectedFontSize))
+})
+fontTypeElem.addEventListener('change', () => {
+	let selectedFontType = fontTypeElem.value
+	vContainer.style.fontFamily = selectedFontType
+
+	localStorage.setItem('fontType', JSON.stringify(selectedFontType))
+})
+
+function loadSavedFontOptions() {
+	const savedFontType = JSON.parse(localStorage.getItem('fontType'))
+	const savedFontSize = JSON.parse(localStorage.getItem('fontSize'))
+
+	if (savedFontSize) {
+		vContainer.style.fontFamily = savedFontType
+	}
+	if (savedFontSize) {
+		vContainer.style.fontSize = savedFontSize
+	}
+}
+loadSavedFontOptions()
