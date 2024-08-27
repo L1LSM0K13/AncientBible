e69aecfdb80fe721934ca39fd19a59f43ff68e89
Omar Mares <omarmares@purelymail.com>
2024-08-27 12:39:03 -0500

    Fixed massive oversite to email verification link

--- src/models/verifyEmail.model.js
+++ src/models/verifyEmail.model.js
@@ -9,7 +9,7 @@ const sgMail = require("@sendgrid/mail");
  */
 const sendVerificationEmail = async (email, verificationToken) => {
 
-    let verificationLink = `https://${process.env.APP_DOMAIN}/verify?token=${verificationToken}`;
+    let verificationLink = `https://${process.env.APP_DOMAIN}/verified?token=${verificationToken}`;
     // let verificationLink = `http://localhost:4000/verified?token=${verificationToken}`;
 
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
@@ -18,12 +18,6 @@ const sendVerificationEmail = async (email, verificationToken) => {
         from: 'nico@ancientbible.org',
         subject: "AncientBible: Verify your email",
         text: `Thank you for signing up with us! Verify your email by clicking this link: ${verificationLink}`,
-        trackingSettings: {
-            clickTracking: {
-                enable: false,
-                enableText: false
-            }
-        }
     }
     sgMail.send(msg).then(() => {console.log('Email Sent')}).catch((err) => {console.error(err)})
 }
