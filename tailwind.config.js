c69b880c31e04f6446740462f844f04de83616b0
Omar Mares <omarmares@purelymail.com>
2024-09-11 17:42:36 -0500

    .

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./public/**/*.{ejs, html, js}"],
+    darkMode: "class",
+    theme: {
+        fontFamily: {
+            sans: ["arial"],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
