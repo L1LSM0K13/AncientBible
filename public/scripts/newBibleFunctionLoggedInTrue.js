06270227df2256e67298e0b80deac72d3d19d3ec
Omar Mares <omarmares@purelymail.com>
2025-04-04 16:38:34 -0500

    Added feature that lets you toggle verse number visibility

--- public/scripts/newBibleFunctionLoggedInTrue.js
+++ public/scripts/newBibleFunctionLoggedInTrue.js
@@ -5,6 +5,7 @@ const vCont = document.getElementById("vCont");
 const bTitle = document.getElementById("bookTitle");
 const bTitle2 = document.getElementById("bookTitle2");
 const redBtn = document.getElementById("redLetterBtn");
+const numberBtn = document.getElementById('numberBtn')
 const nextChapterBtn = document.getElementById("nextChapterBtn1");
 const prevChapterBtn = document.getElementById("prevChapterBtn1");
 
@@ -12,6 +13,7 @@ const nextChapterBtn2 = document.getElementById("nextChapterBtn2");
 const prevChapterBtn2 = document.getElementById("prevChapterBtn2");
 
 let isRed = JSON.parse(localStorage.getItem("isRed")) || false;
+let verseNumberShown = JSON.parse(localStorage.getItem('verseNumberShown')) || false
 /**
  *
  * @param filename {string}
@@ -66,6 +68,9 @@ function loadVerses(verses, container) {
       if (!isRed && verse.isRed) {
         verseText.classList.toggle("text-red-600");
       }
+      if (!verseNumberShown) {
+        verseNumber.classList.toggle("hidden")
+    }
 
       verseNumber.classList.add("p-1");
       verseNumber.innerText = verse.verse;
@@ -318,6 +323,13 @@ async function main() {
   cList.addEventListener("change", async () => {
     await loadChapter(await fetchBook(bList.value), cList.value);
   });
+
+  numberBtn.addEventListener('click', async () => {
+    verseNumberShown = !verseNumberShown
+    localStorage.setItem('verseNumberShown', JSON.stringify(verseNumberShown))
+    await loadChapter(await fetchBook(bList.value), cList.value)
+})
+
 }
 
 main();
